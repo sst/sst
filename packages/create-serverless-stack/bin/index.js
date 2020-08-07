7019a3b954be589328224162108865479bdfe5b3
Jay <jvelayud@gmail.com>
2020-08-06 20:26:32 -0400

    Loading aws-cdk version from npm

--- packages/create-serverless-stack/bin/index.js
+++ packages/create-serverless-stack/bin/index.js
@@ -11,6 +11,7 @@ process.on("unhandledRejection", (err) => {
 
 const fs = require("fs");
 const path = require("path");
+const https = require("https");
 const yargs = require("yargs");
 const chalk = require("chalk");
 const spawn = require("cross-spawn");
@@ -23,54 +24,6 @@ const cmd = {
   i: "create-serverless-stack",
   r: "resources",
 };
-
-const argv = yargs
-  .usage(`${cmd.i} <command>`)
-  .demandCommand(1)
-
-  .command(
-    `${cmd.r} [name]`,
-    "Initialize a template for the resources in your Serverless Stack",
-    function (yargs) {
-      yargs.positional("name", {
-        type: "string",
-        default: "my-serverless-resources",
-        describe: "The name of your Serverless Stack app",
-      });
-    }
-  )
-
-  .option("use-npm", {
-    type: "boolean",
-    default: false,
-    describe: "Use npm instead of Yarn",
-  })
-  .option("language", {
-    type: "string",
-    default: "javascript",
-    choices: ["javascript", "typescript"],
-    describe: "The language of the template",
-  })
-
-  .version()
-  .alias("version", "v")
-  .help("help")
-  .alias("help", "h")
-  .epilogue("For more information, visit www.serverless-stack.com")
-
-  .strictCommands(true)
-  .wrap(yargs.terminalWidth())
-
-  .fail((msg, err) => {
-    if (err) throw err;
-
-    console.log(chalk.red(msg) + "\n");
-
-    yargs.showHelp();
-
-    process.exit(1);
-  })
-  .parse();
 
 function shouldUseYarn() {
   try {
@@ -120,8 +73,128 @@ function copyFiles(sourceDirectory, targetDirectory) {
   }
 }
 
+// Adopted from https://github.com/facebook/create-react-app/blob/master/packages/create-react-app/createReactApp.js
+//
+// We first check the registry directly via the API, and if that fails, we try
+// the slower `npm view [package] version` command.
+//
+// This is important for users in environments where direct access to npm is
+// blocked by a firewall, and packages are provided exclusively via a private
+// registry.
+async function getLatestCdkVersion() {
+  function tryNpmCmd(resolve, reject) {
+    try {
+      resolve(execSync("npm view aws-cdk version").toString().trim());
+    } catch (e) {
+      reject();
+    }
+  }
+
+  return new Promise((resolve, reject) => {
+    https
+      .get("https://registry.npmjs.org/-/package/aws-cdk/dist-tags", (res) => {
+        if (res.statusCode === 200) {
+          let body = "";
+          res.on("data", (data) => (body += data));
+          res.on("end", () => {
+            resolve(JSON.parse(body).latest);
+          });
+        } else {
+          tryNpmCmd(resolve, reject);
+        }
+      })
+      .on("error", () => {
+        tryNpmCmd(resolve, reject);
+      });
+  });
+}
+
+async function main() {
+  try {
+    cdkVersion = await getLatestCdkVersion();
+  } catch (e) {
+    console.log(
+      chalk.red("There was a problem connecting to the npm registry.")
+    );
+    process.exit(1);
+  }
+
+  // Create app directory
+  if (!fs.existsSync(appPath)) {
+    fs.mkdirSync(appPath);
+  } else {
+    console.log(chalk.red(`A directory called ${appName} already exists.`));
+    process.exit(1);
+  }
+
+  // Copy template files to app directory
+  copyFiles(templatePath, appPath);
+
+  // Install dependencies
+  if (useYarn) {
+    spawn.sync("yarn", [], {
+      stdio: "inherit",
+      cwd: appPath,
+    });
+  } else {
+    spawn.sync("npm", ["install"], {
+      stdio: "inherit",
+      cwd: appPath,
+    });
+  }
+}
+
+const argv = yargs
+  .usage(`${cmd.i} <command>`)
+  .demandCommand(1)
+
+  .command(
+    `${cmd.r} [name]`,
+    "Initialize a template for the resources in your Serverless Stack",
+    function (yargs) {
+      yargs.positional("name", {
+        type: "string",
+        default: "my-serverless-resources",
+        describe: "The name of your Serverless Stack app",
+      });
+    }
+  )
+
+  .option("use-npm", {
+    type: "boolean",
+    default: false,
+    describe: "Use npm instead of Yarn",
+  })
+  .option("language", {
+    type: "string",
+    default: "javascript",
+    choices: ["javascript", "typescript"],
+    describe: "The language of the template",
+  })
+
+  .version()
+  .alias("version", "v")
+  .help("help")
+  .alias("help", "h")
+  .epilogue("For more information, visit www.serverless-stack.com")
+
+  .strictCommands(true)
+  .wrap(yargs.terminalWidth())
+
+  .fail((msg, err) => {
+    if (err) throw err;
+
+    console.log(chalk.red(msg) + "\n");
+
+    yargs.showHelp();
+
+    process.exit(1);
+  })
+  .parse();
+
+let cdkVersion;
+
 const appName = argv.name;
-const cdkVersion = "1.56.0";
 const templateType = argv._[0];
 const templateLanguage = argv.language;
 const useYarn = argv.useNpm ? false : shouldUseYarn();
@@ -133,26 +206,4 @@ const templatePath = path.join(
   templateLanguage
 );
 
-// Create app directory
-if (!fs.existsSync(appPath)) {
-  fs.mkdirSync(appPath);
-} else {
-  console.log(chalk.red(`A directory called ${appName} already exists.`));
-  process.exit(1);
-}
-
-// Copy template files to app directory
-copyFiles(templatePath, appPath);
-
-// Install dependencies
-if (useYarn) {
-  spawn.sync("yarn", [], {
-    stdio: "inherit",
-    cwd: appPath,
-  });
-} else {
-  spawn.sync("npm", ["install"], {
-    stdio: "inherit",
-    cwd: appPath,
-  });
-}
+main();
