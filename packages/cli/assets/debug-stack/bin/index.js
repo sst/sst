456de36e57766ef22ba2ef22859fad0e076a9fed
Jay <jvelayud@gmail.com>
2020-12-21 21:24:11 -0500

    Setting the region in the debug stack

--- packages/cli/assets/debug-stack/bin/index.js
+++ packages/cli/assets/debug-stack/bin/index.js
@@ -7,6 +7,9 @@ const stage = process.argv[3];
 const region = process.argv[4];
 const stackName = process.argv[2];
 
+// Override default region
+const env = { account: process.env.CDK_DEFAULT_ACCOUNT, region };
+
 const app = new cdk.App();
 
-new DebugStack(app, stackName, { stage, stackName, region });
+new DebugStack(app, stackName, { env, stage, stackName, region });
