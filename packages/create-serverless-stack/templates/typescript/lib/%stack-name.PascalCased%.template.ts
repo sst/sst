12de81d66229cc49dac709232d1354f56269ba65
Jay <jvelayud@gmail.com>
2021-01-22 19:55:40 -0500

    Updating template with sst.Api

--- packages/create-serverless-stack/templates/typescript/lib/%stack-name.PascalCased%.template.ts
+++ packages/create-serverless-stack/templates/typescript/lib/%stack-name.PascalCased%.template.ts
@@ -1,30 +1,20 @@
 import * as cdk from "@aws-cdk/core";
-import * as apig from "@aws-cdk/aws-apigatewayv2";
-import * as apigIntegrations from "@aws-cdk/aws-apigatewayv2-integrations";
-
 import * as sst from "@serverless-stack/resources";
 
 export default class %stack-name.PascalCased% extends sst.Stack {
   constructor(scope: sst.App, id: string, props?: sst.StackProps) {
     super(scope, id, props);
 
-    // Create a Lambda function triggered by an HTTP API
-    const lambda = new sst.Function(this, "Lambda", {
-      handler: "src/lambda.handler",
-    });
-
     // Create the HTTP API
-    const api = new apig.HttpApi(this, "Api");
-    api.addRoutes({
-      path: "/",
-      integration: new apigIntegrations.LambdaProxyIntegration({
-        handler: lambda,
-      }),
+    const api = new sst.Api(this, "Api", {
+      routes: {
+        "GET /": "src/lambda.handler",
+      },
     });
 
     // Show API endpoint in output
     new cdk.CfnOutput(this, "ApiEndpoint", {
-      value: api.apiEndpoint,
+      value: api.httpApi.apiEndpoint,
     });
   }
 }
