40806bc0fcb813f01298a4175c8f1f6faed4a369
Jay <jvelayud@gmail.com>
2021-01-27 22:55:57 -0500

    Adding missing dependencies for the Api construct

---
+++ packages/cli/test/template-app/lib/MyStack.js
@@ -0,0 +1,20 @@
+import * as cdk from "@aws-cdk/core";
+import * as sst from "@serverless-stack/resources";
+
+export default class MyStack extends sst.Stack {
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    // Create the HTTP API
+    const api = new sst.Api(this, "Api", {
+      routes: {
+        "GET /": "src/lambda.handler",
+      },
+    });
+
+    // Show API endpoint in output
+    new cdk.CfnOutput(this, "ApiEndpoint", {
+      value: api.httpApi.apiEndpoint,
+    });
+  }
+}
---
+++ packages/cli/test/template-app/template-app.test.js
@@ -0,0 +1,31 @@
+const {
+  runJestCommand,
+  runBuildCommand,
+  clearBuildOutput,
+} = require("../helpers");
+
+beforeEach(async () => {
+  await clearBuildOutput(__dirname);
+});
+
+afterAll(async () => {
+  await clearBuildOutput(__dirname);
+});
+
+/**
+ * Test that the synth command ran successfully
+ */
+test("template-app", async () => {
+  const result = await runBuildCommand(__dirname);
+
+  expect(result).toMatch(/Successfully compiled \d+ stack/);
+});
+
+/**
+ * Test that the jest tests run successfully
+ */
+test("template-app-jest", async () => {
+  const result = await runJestCommand(__dirname);
+
+  expect(result).toContain("JESTTESTSUCCESS-----");
+});
