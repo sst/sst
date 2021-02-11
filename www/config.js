1b44f2206f792359cadcef2982afc6957584fb4b
Jay <jvelayud@gmail.com>
2021-02-11 17:46:23 -0500

    Updating Slack links

--- www/config.js
+++ www/config.js
@@ -1,10 +1,10 @@
 module.exports = {
   email: "contact@anoma.ly",
   twitter: "https://twitter.com/ServerlessStack",
+  slack: "https://launchpass.com/serverless-stack",
   forums: "https://discourse.serverless-stack.com/",
   forkedCdk: "https://github.com/serverless-stack/aws-cdk",
   github: "https://github.com/serverless-stack/serverless-stack",
-  slack: "https://communityinviter.com/apps/serverless-stack/invite",
   newsletter:
     "https://emailoctopus.com/lists/1c11b9a8-1500-11e8-a3c9-06b79b628af2/forms/subscribe",
   roadmap:
