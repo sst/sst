b23bacdd586985320ad4ad1482d438424d8570c4
Frank <wangfanjie@gmail.com>
2021-02-11 03:03:01 -0500

    Remove unused rest-api-auth-cognito test

--- packages/cli/test/rest-api-auth-cognito/lib/index.js
+++
@@ -1,7 +0,0 @@
-import MyStack from "./MyStack";
-
-export default function main(app) {
-  new MyStack(app, "my-stack");
-
-  // Add more stacks
-}
--- packages/cli/test/rest-api-auth-cognito/src/public.js
+++
@@ -1,6 +0,0 @@
-export async function main() {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "This is a public route." }),
-  };
-}
