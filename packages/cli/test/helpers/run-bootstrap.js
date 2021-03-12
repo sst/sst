80871324fabb55709324e9de692d1b1310115735
Frank <wangfanjie@gmail.com>
2021-03-12 18:26:18 -0500

    Add tests for Lambda bootstrapper

---
+++ packages/cli/test/helpers/run-bootstrap.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const spawn = require("cross-spawn");
+const paths = require("../../scripts/util/paths");
+
+function runBootstrap(appPath, entry, handler) {
+  let lambdaResponse;
+
+  return new Promise((resolve) => {
+    const event = {};
+    const context = {};
+    const timeoutAt = Date.now() + 3000;
+    const origHandlerPath = "dummy_path";
+
+    const lambda = spawn(
+      path.join(path.dirname(process.execPath), "node"),
+      [
+        path.join(paths.ownPath, "assets", "lambda-invoke", "bootstrap.js"),
+        JSON.stringify(event),
+        JSON.stringify(context),
+        timeoutAt,
+        path.join(appPath, entry),
+        handler,
+        origHandlerPath,
+        paths.appBuildDir,
+      ],
+      {
+        stdio: ["inherit", "inherit", "inherit", "ipc"],
+        cwd: appPath,
+        env: { ...process.env },
+      }
+    );
+
+    function handleResponse(response) {
+      switch (response.type) {
+        case "success":
+        case "failure":
+        case "timeout":
+        case "exit":
+          lambdaResponse = response;
+          break;
+        default:
+      }
+    }
+
+    lambda.on("message", handleResponse);
+    lambda.on("exit", function () {
+      resolve(lambdaResponse);
+    });
+  });
+}
+
+module.exports = runBootstrap;
