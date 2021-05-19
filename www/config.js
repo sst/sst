3eca6deaa936446b8e1b4388955012fd91a56e6c
Jay <jvelayud@gmail.com>
2021-05-18 21:08:36 -0400

    Updating link to newsletter

--- www/config.js
+++ www/config.js
@@ -5,10 +5,9 @@ module.exports = {
   slack: "https://launchpass.com/serverless-stack",
   forum: "https://discourse.serverless-stack.com/",
   forkedCdk: "https://github.com/serverless-stack/aws-cdk",
+  newsletter: "https://serverless-stack.com/newsletter.html",
   examples: "https://serverless-stack.com/examples/index.html",
   github: "https://github.com/serverless-stack/serverless-stack",
-  newsletter:
-    "https://emailoctopus.com/lists/1c11b9a8-1500-11e8-a3c9-06b79b628af2/forms/subscribe",
   roadmap:
     "https://github.com/serverless-stack/serverless-stack/milestones?direction=asc&sort=due_date&state=open",
 };
