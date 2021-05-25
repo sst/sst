5669485bae4de306a5a6e47a8d30fca98930fb21
Frank <wangfanjie@gmail.com>
2021-05-24 23:34:45 -0400

    Add playground example for the Table construct

--- packages/cli/test/playground/lib/index.ts
+++ packages/cli/test/playground/lib/index.ts
@@ -1,17 +1,19 @@
-//import { ApiV1Stack } from "./apiv1-stack";
-//import { MainStack as AppSyncStack } from "./app-sync-api-stack";
 import { MainStack as ApiStack } from "./api-stack";
+//import { MainStack as TableStack } from "./table-stack";
+//import { MainStack as TopicStack } from "./topic-stack";
+//import { MainStack as AppSyncStack } from "./app-sync-api-stack";
 //import { MainStack as ApiWithLambdaAuthStack } from "./api-with-lambda-authorizer";
 //import { MainStack as WebSocketApiStack } from "./websocket-api-stack";
 //import { MainStack as KinesisStreamStack } from "./kinesis-stream";
-import { MainStack as TopicStack } from "./topic-stack";
+//import { ApiV1Stack } from "./apiv1-stack";
 import * as sst from "@serverless-stack/resources";
 
 export default function main(app: sst.App): void {
   //new ApiV1Stack(app, "apiv1");
   //new AppSyncStack(app, "appsync");
   new ApiStack(app, "api");
-  new TopicStack(app, "topic");
+  //new TableStack(app, "table");
+  //new TopicStack(app, "topic");
   //new ApiWithLambdaAuthStack(app, "api-lambda-auth");
   //new WebSocketApiStack(app, "websocket");
   //new KinesisStreamStack(app, "kinesis-stream");
---
+++ packages/cli/test/playground/lib/table-stack.ts
@@ -0,0 +1,29 @@
+import { RemovalPolicy } from "@aws-cdk/core";
+import * as sst from "@serverless-stack/resources";
+
+export class MainStack extends sst.Stack {
+  constructor(scope: sst.App, id: string, props?: sst.StackProps) {
+    super(scope, id, props);
+
+    const table = new sst.Table(this, "Table", {
+      fields: {
+        userId: sst.TableFieldType.STRING,
+      },
+      primaryIndex: { partitionKey: "userId" },
+      dynamodbTable: {
+        removalPolicy: RemovalPolicy.DESTROY,
+      },
+      defaultFunctionProps: {
+        timeout: 3,
+      },
+      stream: true,
+      consumers: {
+        consumerA: "src/lambda.main"
+      },
+    });
+
+    this.addOutputs({
+      TableArn: table.tableArn,
+    });
+  }
+}
