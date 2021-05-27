f466b682f1eb9e070e6d28cae39ab0f7c702eedc
Frank <wangfanjie@gmail.com>
2021-05-27 04:05:10 -0400

    Topic: support Queue subscribers

--- packages/cli/test/playground/lib/index.ts
+++ packages/cli/test/playground/lib/index.ts
@@ -1,6 +1,7 @@
 import { MainStack as ApiStack } from "./api-stack";
 //import { MainStack as TableStack } from "./table-stack";
 //import { MainStack as TopicStack } from "./topic-stack";
+//import { MainStack as AnotherStack } from "./topic-to-queue-stack";
 //import { MainStack as AppSyncStack } from "./app-sync-api-stack";
 //import { MainStack as ApiWithLambdaAuthStack } from "./api-with-lambda-authorizer";
 //import { MainStack as WebSocketApiStack } from "./websocket-api-stack";
@@ -9,12 +10,6 @@ import { MainStack as ApiStack } from "./api-stack";
 import * as sst from "@serverless-stack/resources";
 
 export default function main(app: sst.App): void {
-  //new ApiV1Stack(app, "apiv1");
-  //new AppSyncStack(app, "appsync");
   new ApiStack(app, "api");
-  //new TableStack(app, "table");
-  //new TopicStack(app, "topic");
-  //new ApiWithLambdaAuthStack(app, "api-lambda-auth");
-  //new WebSocketApiStack(app, "websocket");
-  //new KinesisStreamStack(app, "kinesis-stream");
+  //new AnotherStack(app, "another");
 }
