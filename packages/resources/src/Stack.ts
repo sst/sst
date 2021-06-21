7a89dea264c92193856c78bd3f33df42dfbfc306
Frank <wangfanjie@gmail.com>
2021-06-21 03:52:05 -0400

    Fix tests

--- packages/resources/src/Stack.ts
+++ packages/resources/src/Stack.ts
@@ -15,7 +15,7 @@ export class Stack extends cdk.Stack {
     super(scope, stageId, {
       ...props,
       env: {
-        account: root.account,
+        account: process.env.CDK_DEFAULT_ACCOUNT,
         region: root.region,
       },
     });
