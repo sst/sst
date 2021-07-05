88effc5d96ae3bbdd792e6e12931d8715bc6ca26
Frank <wangfanjie@gmail.com>
2021-07-05 16:01:17 -0400

    Remove imported queue

--- packages/cli/test/playground/lib/api-stack.ts
+++ packages/cli/test/playground/lib/api-stack.ts
@@ -1,4 +1,3 @@
-import * as sqs from "@aws-cdk/aws-sqs";
 import * as sst from "@serverless-stack/resources";
 
 export class MainStack extends sst.Stack {
@@ -11,21 +10,7 @@ export class MainStack extends sst.Stack {
       cognito: true,
     });
 
-    const q1 = new sst.Queue(this, "MyQueue", {
-      consumer: "src/lambda.main",
-    });
-
-    new sst.Queue(this, "Queue2", {
-      sqsQueue: q1.sqsQueue,
-      consumer: "src/lambda.main",
-    });
-
-    new sst.Queue(this, "DQueue", {
-      sqsQueue: sqs.Queue.fromQueueArn(
-        this,
-        "IQueue",
-        "arn:aws:sqs:us-east-1:112245769880:delete"
-      ),
+    new sst.Queue(this, "MyQueue", {
       consumer: "src/lambda.main",
     });
 
