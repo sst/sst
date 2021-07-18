aa45668636be47458428b8fa9de8f366a3fa19c8
Lebogang Mabala <39033532+geekmidas@users.noreply.github.com>
2021-07-18 14:55:20 +0200

    Add config test for main entry

---
+++ packages/cli/test/config-main-ts/config-main-ts.spec.js
@@ -0,0 +1,18 @@
+const { runBuildCommand, clearBuildOutput } = require("../helpers");
+
+beforeEach(async () => {
+  await clearBuildOutput(__dirname);
+});
+
+afterAll(async () => {
+  await clearBuildOutput(__dirname);
+});
+
+/**
+ * Test that the config.json is getting picked up
+ */
+test("config", async () => {
+  const result = await runBuildCommand(__dirname);
+
+  expect(result).toContain("krusty-krab-prod-jest-us-west-2");
+});
