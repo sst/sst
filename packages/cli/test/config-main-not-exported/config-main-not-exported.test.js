3027d57f1d56379c501848bfe951aaed09b87c2b
Frank <wangfanjie@gmail.com>
2021-07-21 03:37:14 -0400

    Fix tests

--- packages/cli/test/config-main-not-exported/config-main-not-exported.test.js
+++ packages/cli/test/config-main-not-exported/config-main-not-exported.test.js
@@ -11,7 +11,7 @@ afterAll(async () => {
 /**
  * Test that the config.json is getting picked up
  */
-test("config-main-not-found", async () => {
+test("config-main-not-exported", async () => {
   const result = await runBuildCommand(__dirname);
   expect(result).toMatch(
     /Cannot find app handler. Make sure "infra\/index.js" has a default export./
