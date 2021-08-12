10778edb14df32258803df58bd36bf3646a713e2
Dax Ravi <d@ironbay.co>
2021-08-12 10:35:29 -0400

    dotenv does not accept false as valid value to turn off debugging

--- packages/core/src/util/environment.ts
+++ packages/core/src/util/environment.ts
@@ -16,9 +16,11 @@ export async function load(opts?: LoadOpts) {
     .map((path) => {
       const result = dotenv.config({
         path,
-        debug: process.env.DEBUG !== undefined,
+        debug: process.env.DEBUG !== undefined ? true : undefined,
       });
+      console.log("Done dotenv");
       if (result.error) {
+        // This should throw a normal exception and not assume the process should be killed
         console.error(`Failed to load environment variables from "${path}".`);
         console.error(result.error.message);
         process.exit(1);
