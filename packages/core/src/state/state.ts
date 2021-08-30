a52586562cfe2cc5ee7a4a25c29c77dff6cd1725
Dax Ravi <d@ironbay.co>
2021-08-30 14:46:21 -0400

    Working

---
+++ packages/core/src/state/state.ts
@@ -0,0 +1,41 @@
+import fs from "fs";
+import path from "path";
+import { STS } from "aws-sdk";
+
+export function resolve(root: string, relative: string) {
+  return path.resolve(root, ".sst", relative);
+}
+
+export function init(root: string) {
+  const dir = directory(root);
+  if (fs.existsSync(dir)) return;
+  fs.mkdirSync(dir, { recursive: true });
+}
+
+export function directory(root: string) {
+  return resolve(root, "");
+}
+
+function resolveStage(root: string) {
+  return resolve(root, "stage");
+}
+
+export function getStage(root: string) {
+  const file = resolveStage(root);
+  try {
+    return fs.readFileSync(file).toString();
+  } catch {
+    return null;
+  }
+}
+
+export function setStage(root: string, stage: string) {
+  const file = resolveStage(root);
+  fs.writeFileSync(file, stage);
+}
+
+export async function suggestStage() {
+  const client = new STS();
+  const result = await client.getCallerIdentity().promise();
+  return path.basename(result.Arn!);
+}
