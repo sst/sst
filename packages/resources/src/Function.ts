c504852e885f8121032ee5e8b476f5631f6dd5b2
thdxr <d@ironbay.co>
2021-09-07 19:14:46 -0400

    Manually specify runtime options (#791)

--- packages/resources/src/Function.ts
+++ packages/resources/src/Function.ts
@@ -63,7 +63,24 @@ export interface FunctionProps
    *
    * @default - Defaults to NODEJS_12_X
    */
-  readonly runtime?: string | lambda.Runtime;
+  readonly runtime?:
+    | "nodejs"
+    | "nodejs4.3"
+    | "nodejs6.10"
+    | "nodejs8.10"
+    | "nodejs10.x"
+    | "nodejs12.x"
+    | "nodejs14.x"
+    | "python2.7"
+    | "python3.6"
+    | "python3.7"
+    | "python3.8"
+    | "dotnetcore1.0"
+    | "dotnetcore2.0"
+    | "dotnetcore2.1"
+    | "dotnetcore3.1"
+    | "go1.x"
+    | lambda.Runtime;
   /**
    * The amount of memory in MB allocated.
    *
