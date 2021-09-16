0db9760dd9bdf0446fce53ee55a33b9d35757133
Dax Ravi <d@ironbay.co>
2021-09-16 12:57:12 -0400

    Fixed lint warning

--- examples/typescript/backend/services/sample/index.ts
+++ examples/typescript/backend/services/sample/index.ts
@@ -1,7 +1,7 @@
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 import { Sample } from "@acme/core";
 
-export const handler: APIGatewayProxyHandlerV2 = async (_event) => {
+export const handler: APIGatewayProxyHandlerV2 = async () => {
   const result = Sample.foo();
   return {
     statusCode: 200,
