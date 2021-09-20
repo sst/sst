76fe1d92c4c8ab8303667486389b49cab348c1a8
Frank <wangfanjie@gmail.com>
2021-09-20 06:32:02 -0400

    Fix test

--- packages/resources/test/nextjs-site/pages/env.js
+++ packages/resources/test/nextjs-site/pages/env.js
@@ -6,7 +6,7 @@ export default function Page() {
     <Layout>
       <article>
         <h1 className={utilStyles.headingXl}>Client Side Environment Variable</h1>
-        <h1 className={utilStyles.headingXl}>Env: {process.env.NEXT_PUBLIC_API_URL}</h1>
+        <h1 className={utilStyles.headingXl}>Env: {process.env.API_URL}</h1>
       </article>
     </Layout>
   )
