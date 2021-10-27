ae70912c8e5d4f1323567b7fe736b831d45ee9a5
Dax Raad <d@ironbay.co>
2021-10-27 11:41:37 -0400

    Updates to typescript template (#971)

--- packages/create-serverless-stack/templates/typescript/stacks/index.template.ts
+++ packages/create-serverless-stack/templates/typescript/stacks/index.template.ts
@@ -4,7 +4,7 @@ import * as sst from "@serverless-stack/resources";
 export default function main(app: sst.App): void {
   // Set default runtime for all functions
   app.setDefaultFunctionProps({
-    runtime: "nodejs12.x"
+    runtime: "nodejs14.x"
   });
 
   new %stack-name.PascalCased%(app, "%stack-name%");
