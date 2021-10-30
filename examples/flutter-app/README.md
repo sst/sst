1a02d36bd473b276459e982ca9108e71b63067e0
Manitej66 <pratamaniteja66@gmail.com>
2021-10-30 20:49:11 +0530

    added flutter app

---
+++ examples/flutter-app/README.md
@@ -0,0 +1,45 @@
+# Getting Started with Serverless Stack (SST)
+
+This project was bootstrapped with [Create Serverless Stack](https://docs.serverless-stack.com/packages/create-serverless-stack).
+
+Start by installing the dependencies.
+
+```bash
+$ yarn install
+```
+
+## Commands
+
+### `yarn run start`
+
+Starts the local Lambda development environment.
+
+### `yarn run build`
+
+Build your app and synthesize your stacks.
+
+Generates a `.build/` directory with the compiled files and a `.build/cdk.out/` directory with the synthesized CloudFormation stacks.
+
+### `yarn run deploy [stack]`
+
+Deploy all your stacks to AWS. Or optionally deploy, a specific stack.
+
+### `yarn run remove [stack]`
+
+Remove all your stacks and all of their resources from AWS. Or optionally removes, a specific stack.
+
+### `yarn run test`
+
+Runs your tests using Jest. Takes all the [Jest CLI options](https://jestjs.io/docs/en/cli).
+
+## Documentation
+
+Learn more about the Serverless Stack.
+
+- [Docs](https://docs.serverless-stack.com)
+- [@serverless-stack/cli](https://docs.serverless-stack.com/packages/cli)
+- [@serverless-stack/resources](https://docs.serverless-stack.com/packages/resources)
+
+## Community
+
+[Follow us on Twitter](https://twitter.com/ServerlessStack) or [post on our forums](https://discourse.serverless-stack.com).
---
+++ examples/flutter-app/frontend/lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+
+Future main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  update() async {
+    Uri uri = Uri.parse(dotenv.env['API_URL']!);
+    var result = await http.post(uri);
+    print(result.body);
+    setState(() {
+      counter = int.parse(result.body);
+    });
+    print(counter);
+  }
+
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter serverless-stack demo"),
+        ),
+        body: Container(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("This button is pressed $counter times"),
+                MaterialButton(
+                  onPressed: () {
+                    setState(() {
+                      update();
+                    });
+                  },
+                  child: Text(
+                    "Click Me",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  color: Colors.blue.shade500,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
