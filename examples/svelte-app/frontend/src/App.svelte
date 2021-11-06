e86f71e684068b91527ad00702857b7f1a36442b
Manitej66 <pratamaniteja66@gmail.com>
2021-11-06 20:08:19 +0530

    added svelte app

---
+++ examples/svelte-app/frontend/src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+  let count = 0;
+
+  function onClick() {
+      fetch(import.meta.env.VITE_APP_API_URL, {
+        method: "POST",
+      })
+        .then((response) => response.text())
+        .then((data) => {
+          count = data;
+      });
+    }
+</script>
+
+<div class="App">
+  <p>You clicked me {count} times.</p>
+  <button on:click={onClick}>Click Me!</button>
+</div>
+
+
+<style>
+  .App {
+    text-align: center;
+  }
+  p {
+    margin-top: 0;
+    font-size: 20px;
+  }
+  button {
+    font-size: 48px;
+  }
+</style>
