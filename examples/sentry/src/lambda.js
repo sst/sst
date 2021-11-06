a3fa525c95065563e2f99ab1cbf284a40debd593
Frank <wangfanjie@gmail.com>
2021-11-06 06:54:53 -0400

    Review

--- examples/sentry/src/lambda.js
+++ examples/sentry/src/lambda.js
@@ -1,7 +1,9 @@
-export async function handler(event) {
+import Sentry from "@sentry/serverless";
+
+export const handler = Sentry.AWSLambda.wrapHandler(async (event) => {
   return {
     statusCode: 200,
     headers: { "Content-Type": "text/plain" },
     body: `Hello, World! Your request was received at ${event.requestContext.time}.`,
   };
-}
+});
--- examples/sentry/stacks/MyStack.js
+++ examples/sentry/stacks/MyStack.js
@@ -1,24 +1,17 @@
-import * as sst from "@serverless-stack/resources";
 import { LayerVersion } from "@aws-cdk/aws-lambda";
+import * as sst from "@serverless-stack/resources";
 
 export default class MyStack extends sst.Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
 
-    let sentry = LayerVersion.fromLayerVersionArn(
-      this,
-      "SentryLayer",
-      `arn:aws:lambda:${scope.region}:943013980633:layer:SentryNodeServerlessSDK:35`
-    );
-
-    // Create a HTTP API
-    const api = new sst.Api(this, "Api", {
-      routes: {
-        "GET /": "src/lambda.handler",
-      },
-    });
-
+    // Configure Sentry
     if (!scope.local) {
+      const sentry = LayerVersion.fromLayerVersionArn(
+        this,
+        "SentryLayer",
+        `arn:aws:lambda:${scope.region}:943013980633:layer:SentryNodeServerlessSDK:35`
+      );
       this.addDefaultFunctionLayers([sentry]);
       this.addDefaultFunctionEnv({
         SENTRY_DSN: process.env.SENTRY_DSN,
@@ -26,6 +19,13 @@ export default class MyStack extends sst.Stack {
         NODE_OPTIONS: "-r @sentry/serverless/dist/awslambda-auto",
       });
     }
+
+    // Create a HTTP API
+    const api = new sst.Api(this, "Api", {
+      routes: {
+        "GET /": "src/lambda.handler",
+      },
+    });
 
     // Show the endpoint in the output
     this.addOutputs({
