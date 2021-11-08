8ee18e11fc4679da5319b4f88ab27e3bf42b89e2
Manitej66 <pratamaniteja66@gmail.com>
2021-11-08 17:17:11 +0530

    added middy validation example

---
+++ examples/middy-validation/src/lambda.js
@@ -0,0 +1,50 @@
+import middy from '@middy/core'
+import httpErrorHandler from '@middy/http-error-handler'
+import validator from '@middy/validator'
+import jsonBodyParser from '@middy/http-json-body-parser'
+
+const baseHandler = (event) => {
+  const { fname, lname } = event.body
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "text/plain" },
+    body: `Hello, ${fname + "-" + lname}.`,
+  };
+}
+
+const inputSchema = {
+  type: 'object',
+  properties: {
+    body : {
+      type: 'object',
+      properties: {
+        fname: { type: 'string' },
+        lname: { type: 'string' }
+      },
+      required: ['fname', 'lname'],
+    }
+  },
+}
+
+const outputSchema = {
+  type: 'object',
+  required: ['body', 'statusCode'],
+  properties: {
+    body: {
+      type: 'string'
+    },
+    statusCode: {
+      type: 'number'
+    },
+    headers: {
+      type: 'object'
+    }
+  }
+}
+
+const handler = middy(baseHandler)
+.use(jsonBodyParser())
+.use(validator({ inputSchema: inputSchema, outputSchema: outputSchema }))
+.use(httpErrorHandler())
+
+export { handler }
