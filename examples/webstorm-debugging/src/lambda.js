cb6df8cd55f68437f6b4ec345456ae453a3e580d
Manitej66 <pratamaniteja66@gmail.com>
2021-11-14 17:48:29 +0530

    added example code for debugging in WebStorm

---
+++ examples/webstorm-debugging/src/lambda.js
@@ -0,0 +1,7 @@
+export async function handler(event) {
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "text/plain" },
+    body: `Hello, World! Your request was received at ${event.requestContext.time}.`,
+  };
+}
