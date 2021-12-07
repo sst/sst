27dc0aaaf3901beeba9867bbd8d87a284197c484
Frank <wangfanjie@gmail.com>
2021-12-07 18:10:47 -0500

    Fix prettier

--- packages/cli/test/playground/src/lambda.ts
+++ packages/cli/test/playground/src/lambda.ts
@@ -1,6 +1,8 @@
 import { APIGatewayProxyResult } from "aws-lambda";
 
-export async function main(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
+export async function main(
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> {
   console.log(event);
   return {
     statusCode: 200,
