017f034b8750f2390b4023cd9b8d20116ce4dba8
Maniteja Pratha <pratamaniteja66@gmail.com>
2022-01-04 11:44:34 +0530

    fix: show no buckets sign

--- packages/console/src/App/Stage/Buckets/index.tsx
+++ packages/console/src/App/Stage/Buckets/index.tsx
@@ -1,10 +1,11 @@
 import { NavLink, Route, Routes, Navigate } from "react-router-dom";
-import { Accordion, Scroll } from "~/components";
+import { Accordion, Row, Scroll } from "~/components";
 import { Stack } from "~/components/Stack";
 import { styled } from "~/stitches.config";
 import { useStacks } from "~/data/aws";
 import { Detail } from "./Detail";
 import { Detail as DetailOld } from "./DetailOld";
+import { H3 } from "../components";
 
 const Root = styled("div", {
   display: "flex",
@@ -55,45 +56,54 @@ export function Buckets() {
 
   return (
     <Root>
-      <BucketList>
-        <Scroll.Area>
-          <Scroll.ViewPort>
-            <Accordion.Root
-              type="multiple"
-              defaultValue={stacks.data!.all.map((i) => i.info.StackName!)}
-            >
-              {stacks.data?.all
-                .filter((stack) => (stack.constructs.byType["Bucket"] || []).length > 0)
-                .map((stack) => (
-                  <Accordion.Item
-                    key={stack.info.StackName}
-                    value={stack.info.StackName!}
-                  >
-                    <Accordion.Header>
-                      <Accordion.Trigger>
-                        {stack.info.StackName}
-                        <Accordion.Icon />
-                      </Accordion.Trigger>
-                    </Accordion.Header>
-                    <Accordion.Content>
-                      {stack.constructs.byType["Bucket"]?.map((c) => (
-                        <Bucket key={c.id} to={`${c.data.name}`}>
-                          <Stack space="sm">
-                            <BucketName>{c.id}</BucketName>
-                          </Stack>
-                        </Bucket>
-                      ))}
-                    </Accordion.Content>
-                  </Accordion.Item>
-                ))}
-            </Accordion.Root>
-          </Scroll.ViewPort>
+      {buckets.length === 0 ? (
+        <Row alignHorizontal="center" alignVertical="center">
+          <H3>No buckets found</H3>
+        </Row>
+      ) : (
+        <BucketList>
+          <Scroll.Area>
+            <Scroll.ViewPort>
+              <Accordion.Root
+                type="multiple"
+                defaultValue={stacks.data!.all.map((i) => i.info.StackName!)}
+              >
+                {stacks.data?.all
+                  .filter(
+                    (stack) =>
+                      (stack.constructs.byType["Bucket"] || []).length > 0
+                  )
+                  .map((stack) => (
+                    <Accordion.Item
+                      key={stack.info.StackName}
+                      value={stack.info.StackName!}
+                    >
+                      <Accordion.Header>
+                        <Accordion.Trigger>
+                          {stack.info.StackName}
+                          <Accordion.Icon />
+                        </Accordion.Trigger>
+                      </Accordion.Header>
+                      <Accordion.Content>
+                        {stack.constructs.byType["Bucket"]?.map((c) => (
+                          <Bucket key={c.id} to={`${c.data.name}`}>
+                            <Stack space="sm">
+                              <BucketName>{c.id}</BucketName>
+                            </Stack>
+                          </Bucket>
+                        ))}
+                      </Accordion.Content>
+                    </Accordion.Item>
+                  ))}
+              </Accordion.Root>
+            </Scroll.ViewPort>
 
-          <Scroll.Bar orientation="vertical">
-            <Scroll.Thumb />
-          </Scroll.Bar>
-        </Scroll.Area>
-      </BucketList>
+            <Scroll.Bar orientation="vertical">
+              <Scroll.Thumb />
+            </Scroll.Bar>
+          </Scroll.Area>
+        </BucketList>
+      )}
 
       <Content>
         <Routes>
