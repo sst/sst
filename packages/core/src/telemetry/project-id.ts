bb20046b41e7ce96242eb157cf5ff240ed0543fa
Frank <wangfanjie@gmail.com>
2022-01-13 00:56:06 -0500

    Add tests for telemetry project id parsing (#1255)

--- packages/core/src/telemetry/project-id.ts
+++ packages/core/src/telemetry/project-id.ts
@@ -16,6 +16,43 @@ function getProjectIdByGit() {
   }
 }
 
+export function normalizeGitUrl(url: string): string {
+  // Normalize url
+  // - https://x-access-token:ghs_xxxx@github.com/user/repo.git
+  // - https://github.com/user/repo.git
+  // - git@github.com:user/repo.git
+
+  // trim
+  url = url.trim();
+
+  // clean up ending `.git`
+  url = url.endsWith(".git") ? url.substring(0, url.length - 4) : url;
+
+  if (url.startsWith("git@")) {
+    const match = url.match(/git@([^:]+):(.*)/);
+    if (match && match.length > 2) {
+      return `${match[1]}/${match[2]}`;
+    }
+  } else if (url.startsWith("http://") || url.startsWith("https://")) {
+    const match = url.match(/https?:\/\/([^@]+@)?(.*)/);
+    if (match && match.length > 2) {
+      return match[2];
+    }
+  }
+
+  return url;
+}
+
 export function getRawProjectId(): string {
-  return getProjectIdByGit() || process.env.REPOSITORY_URL || process.cwd();
+  const gitUrl = getProjectIdByGit();
+  if (gitUrl) {
+    return normalizeGitUrl(gitUrl);
+  }
+
+  const repoUrl = process.env.REPOSITORY_URL;
+  if (repoUrl) {
+    return normalizeGitUrl(repoUrl);
+  }
+
+  return process.cwd();
 }
