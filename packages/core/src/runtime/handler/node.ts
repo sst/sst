99aa9a2bcb20206790531f400ad096a2f445f67d
Dax Ravi <d@ironbay.co>
2022-01-26 15:19:31 -0500

    Add ESM hack to esm builds

--- packages/core/src/runtime/handler/node.ts
+++ packages/core/src/runtime/handler/node.ts
@@ -151,6 +151,7 @@ export const NodeHandler: Definition<Bundle> = (opts) => {
         if (existing?.rebuild) {
           const result = await existing.rebuild();
           BUILD_CACHE[opts.id] = result;
+          if (bundle.format === "esm") esmHack(target);
           return [];
         }
         fs.removeSync(artifact);
@@ -166,6 +167,7 @@ export const NodeHandler: Definition<Bundle> = (opts) => {
         fs.writeJSONSync(path.join(artifact, "package.json"), {
           type: bundle.format === "esm" ? "module" : "commonjs",
         });
+        if (bundle.format === "esm") esmHack(target);
         BUILD_CACHE[opts.id] = result;
         return [];
       } catch (e: any) {
@@ -214,6 +216,7 @@ export const NodeHandler: Definition<Bundle> = (opts) => {
       const result = spawn.sync("node", [builder], {
         stdio: "pipe",
       });
+      if (bundle.format === "esm") esmHack(target);
       if (result.status !== 0) {
         const err = (
           result.stderr.toString() + result.stdout.toString()
@@ -455,4 +458,26 @@ function absolutePathToRelativePath(absolutePath: string): string {
   // For posix: root for /path/to/dir is /
   const { root } = path.parse(absolutePath);
   return absolutePath.substring(root.length);
+}
+
+const ESM_HACK_REGEX =
+  /\b__require\("(_http_agent|_http_client|_http_common|_http_incoming|_http_outgoing|_http_server|_stream_duplex|_stream_passthrough|_stream_readable|_stream_transform|_stream_wrap|_stream_writable|_tls_common|_tls_wrap|assert|async_hooks|buffer|child_process|cluster|console|constants|crypto|dgram|diagnostics_channel|dns|domain|events|fs|http|http2|https|inspector|module|net|os|path|perf_hooks|process|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|trace_events|tty|url|util|v8|vm|wasi|worker_threads|zlib)"\)/gm;
+function esmHack(target: string) {
+  const data = fs.readFileSync(target, "utf-8");
+  const modules: Record<string, string> = {};
+  const out = data.replace(ESM_HACK_REGEX, (_, mod) => {
+    const id = "__import_" + mod.toUpperCase();
+    modules[mod] = id;
+    return id;
+  });
+  fs.writeFileSync(
+    target,
+    [
+      ...Object.entries(modules).map(
+        ([key, val]) => `import ${val} from ${JSON.stringify(key)};`
+      ),
+      out,
+    ].join("\n")
+  );
+>>>>>>> 338a6c82 (Add ESM hack to esm builds)
 }
