6e51f30c0408e22febbbff51fe7b81272a722b74
Frank <wangfanjie@gmail.com>
2022-02-05 21:41:13 -0500

    Fix tests

--- packages/resources/bin/copy-rds-migrator.js
+++ packages/resources/bin/copy-rds-migrator.js
@@ -3,5 +3,4 @@
 const fs = require("fs-extra");
 
 fs.rmSync("dist/RDS_migrator", { recursive: true, force: true });
-fs.mkdirp("dist/RDS_migrator");
 fs.copySync("assets/RDS/migrator", "dist/RDS_migrator");
