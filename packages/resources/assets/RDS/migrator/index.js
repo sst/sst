e9583e46c33420c5c87bbf3ecf222ae369ec9ec7
Dax Ravi <d@ironbay.co>
2022-02-14 11:09:13 -0500

    Load migrations dynamically

--- packages/resources/assets/RDS/migrator/index.js
+++ packages/resources/assets/RDS/migrator/index.js
@@ -1,5 +1,7 @@
+"use strict";
+
 import path from "path";
-import { Kysely, FileMigrationProvider, Migrator, NO_MIGRATIONS } from "kysely";
+import { Kysely, Migrator, NO_MIGRATIONS } from "kysely";
 import { DataApiDialect } from "kysely-data-api";
 import RDSDataService from "aws-sdk/clients/rdsdataservice";
 
@@ -18,7 +20,7 @@ export async function handler(evt) {
 
   const migrator = new Migrator({
     db,
-    provider: new FileMigrationProvider(
+    provider: new DynamicFileMigrationProvider(
       path.resolve(process.env.RDS_MIGRATIONS_PATH)
     ),
   });
@@ -39,5 +41,38 @@ export async function handler(evt) {
 
   if (evt.type === "list") {
     return await migrator.getMigrations();
+  }
+}
+
+class DynamicFileMigrationProvider {
+  #migrationFolderPath;
+
+  constructor(migrationFolderPath) {
+    this.#migrationFolderPath = migrationFolderPath;
+  }
+
+  async getMigrations() {
+    // Import these dynamically so that we don't have any top level
+    // node dependencies.
+    const fs = await import("fs/promises");
+    const path = await import("path");
+
+    const migrations = {};
+    const files = await fs.readdir(this.#migrationFolderPath);
+
+    for (const fileName of files) {
+      if (
+        (fileName.endsWith(".js") || fileName.endsWith(".ts")) &&
+        !fileName.endsWith(".d.ts")
+      ) {
+        const migration = await import(
+          path.join(this.#migrationFolderPath, fileName) + "?bust=" + Date.now()
+        );
+
+        migrations[fileName.substring(0, fileName.length - 3)] = migration;
+      }
+    }
+
+    return migrations;
   }
 }
