6057acc07c2c568a16bc4d65d79922b6f45c3b65
Dax Ravi <d@ironbay.co>
2022-03-03 15:53:24 -0500

    The damn icon

--- packages/console/src/App/Stage/Panel.tsx
+++ packages/console/src/App/Stage/Panel.tsx
@@ -6,11 +6,10 @@ import {
   BsFillArchiveFill,
   BsSun,
   BsMoon,
+  BsGlobe2,
 } from "react-icons/bs";
-import { FaDatabase, FaTable } from "react-icons/fa";
+import { FaTable, FaDatabase } from "react-icons/fa";
 import { GrGraphQl } from "react-icons/gr";
-import { SiAmazondynamodb } from "react-icons/si";
-import { AiOutlineConsoleSql } from "react-icons/ai";
 import { Favicon, Logo, Stack } from "~/components";
 import { styled } from "~/stitches.config";
 import { Link, NavLink, useParams } from "react-router-dom";
@@ -189,7 +188,7 @@ export function Panel() {
           <MenuLabel>Buckets</MenuLabel>
         </MenuItem>
         <MenuItem to="api">
-          <GrGraphQl />
+          <BsGlobe2 />
           <MenuLabel>API</MenuLabel>
         </MenuItem>
         <MenuItem to="graphql">
