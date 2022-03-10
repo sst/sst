b2fb530b5b21916a3080ba1e8dc5918c03d3cf47
alistairstead <alistair_stead@me.com>
2022-03-10 17:55:44 +0000

    feat(keepAlive): resolve broken test

--- packages/static-site-env/test/sst-env-keep-alive-file-exists/sst-env-keep-alive-file-exists.test.js
+++ packages/static-site-env/test/sst-env-keep-alive-file-exists/sst-env-keep-alive-file-exists.test.js
@@ -6,6 +6,6 @@ test("sst-env-outputs-file-not-exist", async () => {
   const result = await runStartCommand(__dirname);
 
   expect(result).toContain(
-    "sst-env: Waiting for sst.json to be created. Retry 5"
+    'sst-env: Cannot find the SST outputs file in undefined. Make sure "sst start" is running.'
   );
 }, 6000);
