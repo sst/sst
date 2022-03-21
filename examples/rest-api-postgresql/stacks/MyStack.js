eeaf4f0f3131c4ae3f871d957c4f8eb7f0924ea4
Manitej <61357713+Manitej66@users.noreply.github.com>
2022-03-21 12:18:10 +0530

    Update PostgreSQL example using RDS construct (#1542)

--- examples/rest-api-postgresql/stacks/MyStack.js
+++ examples/rest-api-postgresql/stacks/MyStack.js
@@ -1,55 +1,37 @@
-import * as cdk from "aws-cdk-lib";
-import * as ec2 from "aws-cdk-lib/aws-ec2";
-import * as rds from "aws-cdk-lib/aws-rds";
 import * as sst from "@serverless-stack/resources";
 
 export default class MyStack extends sst.Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
 
-    const defaultDatabaseName = "CounterDB";
+    const DATABASE = "CounterDB";
 
-    // Create the VPC needed for the Aurora Serverless DB cluster
-    const vpc = new ec2.Vpc(this, "CounterVPC");
-
-    // Create the Serverless Aurora DB cluster
-    const cluster = new rds.ServerlessCluster(this, "CounterDBCluster", {
-      vpc,
-      defaultDatabaseName,
-      // Set the engine to Postgres
-      engine: rds.DatabaseClusterEngine.AURORA_POSTGRESQL,
-      parameterGroup: rds.ParameterGroup.fromParameterGroupName(
-        this,
-        "ParameterGroup",
-        "default.aurora-postgresql10"
-      ),
-      // Optional, disable the instance from pausing after 5 minutes
-      scaling: { autoPause: cdk.Duration.seconds(0) },
+    // Create the Aurora DB cluster
+    const cluster = new sst.RDS(this, "Cluster", {
+      engine: "postgresql10.14",
+      defaultDatabaseName: DATABASE,
+      migrations: "src/migrations",
     });
 
     // Create a HTTP API
     const api = new sst.Api(this, "Api", {
-      routes: {
-        "POST /": {
-          function: {
-            handler: "src/lambda.handler",
-            environment: {
-              dbName: defaultDatabaseName,
-              clusterArn: cluster.clusterArn,
-              secretArn: cluster.secret.secretArn,
-            },
-          },
+      defaultFunctionProps: {
+        environment: {
+          DATABASE,
+          CLUSTER_ARN: cluster.clusterArn,
+          SECRET_ARN: cluster.secretArn,
         },
+        permissions: [cluster],
+      },
+      routes: {
+        "POST /": "src/lambda.handler",
       },
     });
-
-    // Grant access to the cluster from the Lambda function
-    cluster.grantDataApiAccess(api.getFunction("POST /"));
 
     // Show the resource info in the output
     this.addOutputs({
       ApiEndpoint: api.url,
-      SecretArn: cluster.secret.secretArn,
+      SecretArn: cluster.secretArn,
       ClusterIdentifier: cluster.clusterIdentifier,
     });
   }
