9d830d97d537b76c6048e46bc5b5dd14a87c8815
Dax Ravi <d@ironbay.co>
2022-04-06 10:47:58 -0400

    Sentry sourcemaps

--- packages/console/vite.config.ts
+++ packages/console/vite.config.ts
@@ -1,13 +1,34 @@
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
+import viteSentry from "vite-plugin-sentry";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteSentry({
+      debug: true,
+      authToken: process.env.SENTRY_TOKEN,
+      org: "serverless_stack",
+      project: "console",
+      deploy: {
+        env: "production",
+      },
+      setCommits: {
+        auto: true,
+      },
+      sourceMaps: {
+        include: ["./dist"],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    sourcemap: true,
   },
 });
