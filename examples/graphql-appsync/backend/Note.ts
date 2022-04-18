885b1bf74d640863fd6489e3f129e8c37369d144
Dax Ravi <d@ironbay.co>
2022-04-17 20:53:01 -0400

    Examples: Use v1 path for examples

--- examples/graphql-appsync/backend/Note.ts
+++
@@ -1,6 +0,0 @@
-type Note = {
-  id: string;
-  content: string;
-};
-
-export default Note;
--- examples/rest-api-csharp/stacks/index.ts
+++
@@ -1,9 +0,0 @@
-import { MyStack } from "./MyStack";
-
-export default function main(app) {
-  app.setDefaultFunctionProps({
-    runtime: "dotnetcore3.1",
-  });
-  app.stack(MyStack);
-  // Add more stacks
-}
