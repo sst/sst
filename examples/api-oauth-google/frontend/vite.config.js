5372aa430c9d284d17e8cced6b3a709529ba98a5
Maniteja Pratha <pratamaniteja66@gmail.com>
2022-04-22 11:52:25 +0530

    Update examples to latest version of SST

--- examples/api-oauth-google/frontend/vite.config.js
+++ examples/api-oauth-google/frontend/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "./runtimeConfig": "./runtimeConfig.browser",
+    },
+  },
+});
