853894fb9884da9d418ab13a24f0e208c3eddb8c
Dax Ravi <d@ironbay.co>
2022-05-03 15:18:39 -0400

    Don't use beta for templates

--- packages/create-sst/bin/presets/base/javascript/preset.mjs
+++ packages/create-sst/bin/presets/base/javascript/preset.mjs
@@ -3,10 +3,7 @@ import { extract, patch, install } from "create-sst";
 export default [
   extract(),
   install({
-    packages: [
-      "@serverless-stack/cli@1.0.0-beta.9",
-      "@serverless-stack/resources@1.0.0-beta.9",
-    ],
+    packages: ["@serverless-stack/cli", "@serverless-stack/resources"],
     dev: true,
   }),
 ];
