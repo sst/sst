7a1fc1ca1b41cf99c778edb39787c3f7542ba50f
Frank <wangfanjie@gmail.com>
2022-05-31 12:15:27 -0400

    Use cjs format for the Apollo example

--- packages/create-sst/bin/presets/examples/graphql-apollo/templates/stacks/MyStack.ts
+++ packages/create-sst/bin/presets/examples/graphql-apollo/templates/stacks/MyStack.ts
@@ -3,7 +3,12 @@ import { GraphQLApi, StackContext } from "@serverless-stack/resources";
 export function MyStack({ stack }: StackContext) {
   // Create the GraphQL API
   const api = new GraphQLApi(stack, "ApolloApi", {
-    server: "functions/lambda.handler",
+    server: {
+      handler: "functions/lambda.handler",
+      bundle: {
+        format: "cjs",
+      },
+    },
   });
 
   // Show the API endpoint in output
