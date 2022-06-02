f2f887bbdec1e45e618da5a53ca526d95012285f
Dax Ravi <d@ironbay.co>
2022-06-02 19:56:48 -0400

    RDS: Fix migrations not showing up when deployed

---
+++ .changeset/nice-lies-crash.md
@@ -0,0 +1,5 @@
+---
+"@serverless-stack/resources": patch
+---
+
+Fix for .mjs migrations not showing up when RDS is deployed
--- packages/resources/src/RDS.ts
+++ packages/resources/src/RDS.ts
@@ -404,6 +404,7 @@ export class RDS extends Construct implements SSTConstruct {
         RDS_MIGRATIONS_PATH: app.local ? migrations : migrationsDestination
       },
       bundle: {
+        nodeModules: ["kysely", "kysely-data-api"],
         // Note that we need to generate a relative path of the migrations off the
         // srcPath because sst.Function internally builds the copy "from" path by
         // joining the srcPath and the from path.
