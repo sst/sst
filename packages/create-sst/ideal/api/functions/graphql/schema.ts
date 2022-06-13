3becb3d5994dc755b2713757fb661f9b51000bad
Dax Ravi <d@ironbay.co>
2022-06-13 14:21:48 -0400

    Remove accidentally synthesized ideal stack

--- packages/create-sst/ideal/api/functions/graphql/schema.ts
+++
@@ -1,5 +0,0 @@
-import { builder } from "./builder";
-
-import "./types/article";
-
-export const schema = builder.toSchema({});
--- packages/create-sst/ideal/web/src/sst-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_GRAPHQL_URL: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
