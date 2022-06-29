00cbd4931be2dd0256efed7e51d3f857a0c79fde
Frank <wangfanjie@gmail.com>
2022-06-29 15:44:53 -0400

    Update config.js

--- www/config.js
+++ www/config.js
@@ -9,7 +9,7 @@ module.exports = {
   console: "https://console.sst.dev/",
   slack_invite: "https://sst.dev/discord",
   discord_invite: "https://sst.dev/discord",
-  youtube: "https://www.youtube.com/c/ServerlessStack",
+  youtube: "https://www.youtube.com/c/watch-sst",
   newsletter: "https://sst.dev/newsletter.html",
   examples: "https://sst.dev/examples/index.html",
   github: "https://github.com/serverless-stack/sst",
