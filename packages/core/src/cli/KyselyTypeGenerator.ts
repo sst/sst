1533bd42b7dfc318a77cbd4d0878f0c052bdcb59
Dax Ravi <d@ironbay.co>
2022-07-18 09:46:55 -0400

    Use kysely-codegen

--- packages/core/src/cli/KyselyTypeGenerator.ts
+++ packages/core/src/cli/KyselyTypeGenerator.ts
@@ -1,10 +1,12 @@
 import { Bus } from "./Bus.js";
 import { Config } from "../config/index.js";
-import { toTypeScript, toObject } from "@rmp135/sql-ts";
-import fs from "fs/promises";
-import knex from "knex";
-/* @ts-ignore */
-import knexDataApiClient from "knex-aurora-data-api-client";
+import { CodegenSerializer } from "kysely-codegen";
+import { CodegenDialectManager } from "kysely-codegen/dist/dialect-manager";
+import { Kysely } from "kysely";
+import { DataApiDialect } from "kysely-data-api";
+import { RDSDataService } from "aws-sdk";
+import { CodegenFormat } from "kysely-codegen/dist/enums/format";
+import * as fs from "fs/promises";
 
 interface Opts {
   bus: Bus;
@@ -22,52 +24,49 @@ interface Database {
 export function createKyselyTypeGenerator(opts: Opts) {
   let databases: Database[] = [];
 
-  opts.bus.subscribe("stacks.deployed", (evt) => {
+  opts.bus.subscribe("stacks.deployed", evt => {
     databases = evt.properties.metadata
-      .filter((c) => c.type === "RDS")
-      .filter((c) => c.data.migrator)
-      .filter((c) => c.data.types)
-      .map((c) => ({
+      .filter(c => c.type === "RDS")
+      .filter(c => c.data.migrator)
+      .filter(c => c.data.types)
+      .map(c => ({
         migratorID: evt.properties.metadata.find(
-          (fn) => fn.addr == c.data.migrator?.node
+          fn => fn.addr == c.data.migrator?.node
         ).data.localId,
         clusterArn: c.data.clusterArn,
         types: c.data.types,
         engine: c.data.engine,
         defaultDatabaseName: c.data.defaultDatabaseName,
-        secretArn: c.data.secretArn,
+        secretArn: c.data.secretArn
       }));
   });
 
-  opts.bus.subscribe("function.responded", async (evt) => {
-    const db = databases.find((db) => db.migratorID === evt.properties.localID);
+  opts.bus.subscribe("function.responded", async evt => {
+    const db = databases.find(db => db.migratorID === evt.properties.localID);
     if (!db) return;
     if (!db.types) return;
     if (evt.properties.request.event.type !== "to") return;
 
-    const k = knex({
-      client: db.engine.includes("mysql")
-        ? knexDataApiClient.mysql
-        : knexDataApiClient.postgres,
-      connection: {
-        secretArn: db.secretArn,
-        resourceArn: db.clusterArn,
-        database: db.defaultDatabaseName,
-        region: opts.config.region,
-      } as any,
+    const k = new Kysely<Database>({
+      dialect: new DataApiDialect({
+        mode: db.engine as any,
+        driver: {
+          secretArn: process.env.RDS_SECRET_ARN!,
+          resourceArn: process.env.RDS_ARN!,
+          database: process.env.RDS_DATABASE!,
+          client: new RDSDataService({
+            region: opts.config.region
+          })
+        }
+      })
     });
-
-    const result = await toTypeScript(
-      {
-        interfaceNameFormat: "${table}",
-      },
-      k as any
-    );
-    const lines = [result, "export interface Database {"];
-    for (const match of result.matchAll(/export interface ([^\s]+)\s/g)) {
-      lines.push(`  "${match[1]}": ${match[1]}`);
-    }
-    lines.push("}");
-    fs.writeFile(db.types, lines.join("\n"));
+    const tables = await k.introspection.getTables();
+    const serializr = new CodegenSerializer({
+      dialect: new CodegenDialectManager().getDialect(db.engine as any),
+      format: CodegenFormat.INTERFACE,
+      tables
+    });
+    const data = serializr.serialize();
+    await fs.writeFile(db.types!, data);
   });
 }
