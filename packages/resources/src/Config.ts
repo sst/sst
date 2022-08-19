c47254fd49f755ddc83804d9f6e6cbac92bbf763
Kevin Wright <kevin86wright@gmail.com>
2022-08-19 12:05:10 -0600

    Updating ConfigType properties to be readonly, discourage the changing of these values at runtime within lambda, which would cause problems on consecutive invocations of a lambda runtime

--- packages/resources/src/Config.ts
+++ packages/resources/src/Config.ts
@@ -19,12 +19,9 @@ export function codegenTypes() {
     import "@serverless-stack/node/config";
     declare module "@serverless-stack/node/config" {
     export interface ConfigType {
-      ${[
-        "APP",
-        "STAGE",
-        ...Parameter.getAllNames(),
-        ...Secret.getAllNames()
-      ].map((p) => `${p}: string;`).join("\n")}
+      ${["APP", "STAGE", ...Parameter.getAllNames(), ...Secret.getAllNames()]
+        .map((p) => `readonly ${p}: string;`)
+        .join("\n")}
     }
   }`
   );
