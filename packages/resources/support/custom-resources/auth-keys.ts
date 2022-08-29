80c6beb72bdd85606cad83ddcfbd4e34232dd95a
Dax Ravi <d@ironbay.co>
2022-08-29 11:29:19 -0400

    Handle deletes

--- packages/resources/support/custom-resources/auth-keys.ts
+++ packages/resources/support/custom-resources/auth-keys.ts
@@ -1,37 +1,64 @@
-import { SSMClient, PutParameterCommand } from "@aws-sdk/client-ssm";
+import {
+  SSMClient,
+  DeleteParameterCommand,
+  PutParameterCommand,
+} from "@aws-sdk/client-ssm";
 import crypto from "crypto";
 import { promisify } from "util";
 
 const generateKeyPair = promisify(crypto.generateKeyPair);
 export async function AuthKeys(cfnRequest: any) {
   const { privatePath, publicPath } = cfnRequest.ResourceProperties;
-
-  const { publicKey, privateKey } = await generateKeyPair("rsa", {
-    modulusLength: 2048, // the length of your key in bits
-    publicKeyEncoding: {
-      type: "spki",
-      format: "pem",
-    },
-    privateKeyEncoding: {
-      type: "pkcs8",
-      format: "pem",
-    },
-  });
   const client = new SSMClient({});
 
-  await client.send(
-    new PutParameterCommand({
-      Name: privatePath,
-      Value: privateKey,
-      Type: "SecureString",
-    })
-  );
+  switch (cfnRequest.RequestType) {
+    case "Create":
+      const { publicKey, privateKey } = await generateKeyPair("rsa", {
+        modulusLength: 2048, // the length of your key in bits
+        publicKeyEncoding: {
+          type: "spki",
+          format: "pem",
+        },
+        privateKeyEncoding: {
+          type: "pkcs8",
+          format: "pem",
+        },
+      });
+      await Promise.all([
+        client.send(
+          new PutParameterCommand({
+            Name: privatePath,
+            Value: privateKey,
+            Type: "SecureString",
+          })
+        ),
 
-  await client.send(
-    new PutParameterCommand({
-      Name: publicPath,
-      Value: publicKey,
-      Type: "SecureString",
-    })
-  );
+        client.send(
+          new PutParameterCommand({
+            Name: publicPath,
+            Value: publicKey,
+            Type: "SecureString",
+          })
+        ),
+      ]);
+      break;
+    case "Update":
+      break;
+    case "Delete":
+      await Promise.all([
+        client.send(
+          new DeleteParameterCommand({
+            Name: privatePath,
+          })
+        ),
+        client.send(
+          new DeleteParameterCommand({
+            Name: publicPath,
+          })
+        ),
+      ]);
+      break;
+    default:
+      throw new Error("Unsupported request type");
+  }
 }
