9a96b17530542e7549ee6d857f5f678229db2383
Dax Ravi <d@ironbay.co>
2022-09-23 10:04:46 -0400

    Logger

--- packages/cli2/src/stacks/build.ts
+++ packages/cli2/src/stacks/build.ts
@@ -3,6 +3,7 @@ import esbuild from "esbuild";
 import fs from "fs/promises";
 import path from "path";
 import { useState } from "../state/index.js";
+import { Logger } from "../logger/index.js";
 
 export async function build() {
   import("aws-cdk-lib");
@@ -10,10 +11,11 @@ export async function build() {
   const state = await useState();
   const config = await useConfig();
   const pkg = JSON.parse(
-    await fs.readFile(path.join(root, "package.json")).then(x => x.toString())
+    await fs.readFile(path.join(root, "package.json")).then((x) => x.toString())
   );
   const outfile = path.join(state, `stacks.${Math.random()}.mjs`);
 
+  Logger.debug("Running esbuild on", config.main, "to", outfile);
   await esbuild.build({
     keepNames: true,
     bundle: true,
@@ -27,26 +29,29 @@ export async function build() {
       ...Object.keys({
         ...pkg.devDependencies,
         ...pkg.dependencies,
-        ...pkg.peerDependencies
-      })
+        ...pkg.peerDependencies,
+      }),
     ],
     absWorkingDir: root,
     outfile,
     banner: {
       js: [
         `import { createRequire as topLevelCreateRequire } from 'module';`,
-        `const require = topLevelCreateRequire(import.meta.url);`
-      ].join("")
+        `const require = topLevelCreateRequire(import.meta.url);`,
+      ].join(""),
     },
     // The entry can have any file name (ie. "stacks/anything.ts"). We want the
     // build output to be always named "lib/index.js". This allow us to always
     // import from "buildDir" without needing to pass "anything" around.
-    entryPoints: [config.main]
+    entryPoints: [config.main],
   });
+  Logger.debug("Finished esbuild");
 
+  Logger.debug("Sourcing stacks");
   const mod = await import(outfile);
+  Logger.debug("Finished sourcing stacks");
   await fs.rm(outfile, {
-    force: true
+    force: true,
   });
   return mod.default;
 }
