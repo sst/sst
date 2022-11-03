f54cb8240b657e8a168d97899ab5484837f3fcd6
Dax Ravi <d@ironbay.co>
2022-11-02 22:49:22 -0400

    Sync

--- packages/cli2/src/cli/commands/start.tsx
+++ packages/cli2/src/cli/commands/start.tsx
@@ -21,9 +21,11 @@ export const start = (program: Program) =>
   program.command(
     "start",
     "Work on your SST app locally",
-    (yargs) => yargs,
+    yargs => yargs,
     async () => {
-      ora("Listening for function invocations").start().succeed();
+      ora("Listening for function invocations")
+        .start()
+        .succeed();
       await Promise.all([
         useRuntimeWorkers(),
         useIOTBridge(),
@@ -31,7 +33,7 @@ export const start = (program: Program) =>
         useNodeHandler(),
         useMetadata(),
         useFunctionLogger(),
-        useStackBuilder(),
+        useStackBuilder()
       ]);
     }
   );
@@ -39,55 +41,104 @@ export const start = (program: Program) =>
 const useFunctionLogger = Context.memo(async () => {
   const bus = useBus();
 
-  bus.subscribe("function.invoked", async (evt) => {
+  bus.subscribe("function.invoked", async evt => {
     const functions = await useFunctions();
     const func = functions[evt.properties.functionID];
     console.log(bold(magenta(`Invoked `)), bold(func.id));
   });
 
-  bus.subscribe("worker.stdout", async (evt) => {
+  bus.subscribe("worker.stdout", async evt => {
     const functions = await useFunctions();
     const func = functions[evt.properties.functionID];
     console.log(bold(blue(`Log     `)), bold(func.id), evt.properties.message);
   });
 
-  bus.subscribe("function.success", async (evt) => {
+  bus.subscribe("function.success", async evt => {
     const functions = await useFunctions();
     const func = functions[evt.properties.functionID];
     console.log(bold(green(`Success `)), bold(func.id));
   });
 });
 
+import path from "path";
+import fs from "fs/promises";
+import crypto from "crypto";
+import { CloudAssembly } from "aws-cdk-lib/cx-api";
+
 const useStackBuilder = Context.memo(async () => {
   const watcher = useWatcher();
   const bus = useBus();
 
+  let checksum: string;
+  let pending: CloudAssembly | undefined;
+
   async function build() {
+    const spinner = ora("Building stacks").start();
     const fn = await Stacks.build();
     const assembly = await Stacks.synth({
       fn,
-      mode: "start",
+      mode: "start"
     });
+    Logger.debug("Directory", assembly.directory);
+    const next = await generateChecksum(assembly.directory);
+    Logger.debug("Checksum", "next", next, "old", checksum);
+    if (next === checksum) {
+      spinner.succeed("No changes");
+      return;
+    }
+    checksum = next;
+    pending = assembly;
+    spinner.succeed(`Stacks built! Press ${blue("enter")} to deploy`);
+  }
+
+  async function deploy() {
+    if (!pending) return;
     process.stdout.write("\x1b[?1049h");
     const component = render(
-      <DeploymentUI stacks={assembly.stacks.map((s) => s.stackName)} />
+      <DeploymentUI stacks={pending.stacks.map(s => s.stackName)} />
     );
-    await Stacks.deployMany(assembly.stacks);
+    await Stacks.deployMany(pending.stacks);
     component.unmount();
     process.stdout.write("\x1b[?1049l");
-    console.log("Stacks updated");
+    pending = undefined;
+  }
+
+  async function generateChecksum(cdkOutPath: string) {
+    const manifestPath = path.join(cdkOutPath, "manifest.json");
+    const cdkManifest = JSON.parse(
+      await fs.readFile(manifestPath).then(x => x.toString())
+    );
+    const checksumData = await Promise.all(
+      Object.keys(cdkManifest.artifacts)
+        .filter(
+          (key: string) =>
+            cdkManifest.artifacts[key].type === "aws:cloudformation:stack"
+        )
+        .map(async (key: string) => {
+          const { templateFile } = cdkManifest.artifacts[key].properties;
+          const templatePath = path.join(cdkOutPath, templateFile);
+          const templateContent = await fs.readFile(templatePath);
+          return templateContent;
+        })
+    ).then(x => x.join("\n"));
+    const hash = crypto
+      .createHash("sha256")
+      .update(checksumData)
+      .digest("hex");
+    return hash;
   }
 
   let metafile: Metafile;
-  bus.subscribe("stack.built", async (evt) => {
+  bus.subscribe("stack.built", async evt => {
     metafile = evt.properties.metafile;
   });
 
-  watcher.subscribe("file.changed", async (evt) => {
+  watcher.subscribe("file.changed", async evt => {
     if (!metafile) return;
     if (!metafile.inputs[evt.properties.relative]) return;
     build();
   });
 
   await build();
+  await deploy();
 });
