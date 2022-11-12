646e967df9d1d15535fc077b8d1513341241e0b5
Martin Granell <925072+mgranell@users.noreply.github.com>
2022-11-12 13:37:48 +1100

    Change gradle build to explicit gradle env var

--- packages/core/src/runtime/handler/java.ts
+++ packages/core/src/runtime/handler/java.ts
@@ -36,8 +36,9 @@ export const JavaHandler: Definition<Bundle> = opts => {
     args: [
       buildTask,
       `-Dorg.gradle.project.buildDir=${target}`,
-      `-Dorg.gradle.logging.level=${process.env.DEBUG ? "debug" : "lifecycle"}`,
+      ...(process.env.GRADLE_LOGGING_LEVEL ? [`-Dorg.gradle.logging.level=${process.env.GRADLE_LOGGING_LEVEL}`] : []),
     ],
+
     env: {},
   };
 
