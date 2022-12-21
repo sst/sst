a0b02579490d9386fc4b5db3d895bff7096cffb7
Dax Ravi <d@ironbay.co>
2022-12-21 00:01:14 -0500

    Remove static-site-env

--- packages/static-site-env/test/base/base.test.js
+++
@@ -1,9 +0,0 @@
-import { runStartCommand } from "../helpers";
-import { test, expect } from "vitest";
-
-test("base", async () => {
-  const result = await runStartCommand(__dirname);
-
-  expect(result).not.toContain("Waiting for SST to start");
-  expect(result).toContain("my-url");
-});
