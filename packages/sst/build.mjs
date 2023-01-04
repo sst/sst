6d97eef83f61a474d6c43d3d8f962fbd147c0e10
Frank <wangfanjie@gmail.com>
2023-01-04 14:32:51 -0500

    NextjsSite: add a helper function for zipping server function bundle

--- packages/sst/build.mjs
+++ packages/sst/build.mjs
@@ -183,5 +183,29 @@ await esbuild.build({
   outfile: "./dist/support/base-site-archiver.mjs",
 });
 
+await fs.cp(
+  "support/ssr-site-function-archiver.cjs",
+  "dist/support/ssr-site-function-archiver.cjs",
+  {
+    recursive: true,
+  }
+);
+await esbuild.build({
+  keepNames: true,
+  bundle: true,
+  minify: true,
+  platform: "node",
+  target: "esnext",
+  format: "esm",
+  entryPoints: ["./support/ssr-site-function-archiver.cjs"],
+  banner: {
+    js: [
+      `import { createRequire as topLevelCreateRequire } from 'module';`,
+      `const require = topLevelCreateRequire(import.meta.url);`,
+    ].join(""),
+  },
+  outfile: "./dist/support/ssr-site-function-archiver.mjs",
+});
+
 console.log("Built");
 if (watch) console.log("Watching");
