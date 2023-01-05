e99acfda77aac87c703e57af114c45a394686136
Andy Nguyen <andynguyenswe@gmail.com>
2023-01-05 08:17:10 -0800

    console: Enable CMD/Ctrl + Enter to submit form (#2298)

--- packages/console/src/App/Stage/Functions/Detail.tsx
+++ packages/console/src/App/Stage/Functions/Detail.tsx
@@ -149,7 +149,14 @@ const Invoke = memo((props: { metadata: FunctionMetadata }) => {
 
   return (
     <InvokeRoot>
-      <form onSubmit={onSubmit}>
+      <form
+        onSubmit={onSubmit}
+        onKeyDown={(e) => {
+          if (e.key === "Enter" && e.metaKey) {
+            onSubmit();
+          }
+        }}
+      >
         <InvokeTextarea
           maxRows={20}
           minRows={5}
