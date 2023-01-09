413381061bea3c6622240a2eff264c3733cd02cf
Dax Ravi <d@ironbay.co>
2023-01-09 10:38:43 -0500

    Proper template for sst2

--- packages/create-sst/bin/presets/standard/base/templates/packages/functions/src/lambda.ts
+++ packages/create-sst/bin/presets/standard/base/templates/packages/functions/src/lambda.ts
@@ -1,1 +1,8 @@
-import { sst } from "sst/
+import { ApiHandler } from "sst/node/api";
+import { Time } from "@my-sst-app/core/time";
+
+export const handler = ApiHandler(async (_evt) => {
+  return {
+    body: `Hello world. The time is ${Time.now()}`,
+  };
+});
