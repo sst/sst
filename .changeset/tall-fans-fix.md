16a936e38e56f5f88c5de7a9c07527949d36f7be
Justin Rahardjo <justindra91@gmail.com>
2023-03-16 19:00:46 -0700

    chore: exported the adapter type for future auth (#2677)

---
+++ .changeset/tall-fans-fix.md
@@ -0,0 +1,5 @@
+---
+"sst": patch
+---
+
+Exported the Adapter type for future auth, so others can create their own custom adapters
--- packages/sst/src/node/future/auth/index.ts
+++ packages/sst/src/node/future/auth/index.ts
@@ -10,6 +10,7 @@ export * from "./adapter/google.js";
 export * from "./adapter/link.js";
 export * from "./adapter/github.js";
 export * from "./adapter/oauth.js";
+export type { Adapter } from "./adapter/adapter.js";
 
 export * from "./session.js";
 export * from "./handler.js";
