28979fa26e5860b9cb55c106576c1543b7f3486e
Jay V <air@live.ca>
2023-03-30 18:34:10 -0400

    Adding quick start nextjs repo

---
+++ packages/create-sst/bin/presets/examples/quickstart-nextjs/templates/sst.config.ts
@@ -0,0 +1,34 @@
+import { SSTConfig } from "sst";
+import { Cron, Bucket, NextjsSite } from "sst/constructs";
+
+export default {
+  config(_input) {
+    return {
+      name: "@@app",
+      region: "us-east-1",
+    };
+  },
+  stacks(app) {
+    app.stack(function Site({ stack }) {
+      const bucket = new Bucket(stack, "public", {
+        cors: true,
+      });
+      const site = new NextjsSite(stack, "site", {
+        bind: [bucket],
+      });
+      new Cron(stack, "cron", {
+        schedule: "rate(1 minute)",
+        job: {
+          function: {
+            bind: [bucket],
+            handler: "functions/delete.handler",
+          },
+        },
+      });
+
+      stack.addOutputs({
+        SiteUrl: site.url,
+      });
+    });
+  },
+} satisfies SSTConfig;
