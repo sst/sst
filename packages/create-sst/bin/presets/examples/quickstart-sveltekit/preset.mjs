9821fcfb5d95bc74f9351d8c34f5d4db857de7b1
Jay V <air@live.ca>
2023-05-08 17:33:33 -0400

    Adding svelte quick start example

---
+++ packages/create-sst/bin/presets/examples/quickstart-sveltekit/preset.mjs
@@ -0,0 +1,25 @@
+import { patch, extend, extract, install, cmd, str_replace } from "create-sst";
+export default [
+  extract(),
+  install({
+    packages: [
+      "@aws-sdk/client-s3",
+      "@aws-sdk/s3-request-presigner",
+      "@fontsource/fira-mono",
+      "@neoconfetti/svelte",
+      "@sveltejs/adapter-auto",
+      "@sveltejs/kit",
+      "@types/cookie",
+      "aws-cdk-lib@2.72.1",
+      "constructs@10.1.156",
+      "sst",
+      "svelte",
+      "svelte-check",
+      "svelte-kit-sst",
+      "tslib",
+      "typescript",
+      "vite",
+    ],
+    dev: true,
+  }),
+];
---
+++ packages/create-sst/bin/presets/examples/quickstart-sveltekit/templates/sst.config.ts
@@ -0,0 +1,32 @@
+import type { SSTConfig } from "sst";
+import { Cron, Bucket, SvelteKitSite } from "sst/constructs";
+
+export default {
+  config(_input) {
+    return {
+      name: "@@app",
+      region: "us-east-1",
+    };
+  },
+  stacks(app) {
+    app.stack(function Site({ stack }) {
+      const bucket = new Bucket(stack, "public");
+      const site = new SvelteKitSite(stack, "site", {
+        bind: [bucket],
+      });
+      new Cron(stack, "cron", {
+        schedule: "rate(1 day)",
+        job: {
+          function: {
+            bind: [bucket],
+            handler: "src/functions/delete.handler",
+          },
+        },
+      });
+
+      stack.addOutputs({
+        url: site.url,
+      });
+    });
+  },
+} satisfies SSTConfig;
