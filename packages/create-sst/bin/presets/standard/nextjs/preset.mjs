db7a9c5931df8b4569900d8ca711486f12b7c9bd
Jay V <air@live.ca>
2023-05-09 16:56:05 -0400

    Updating next preset

--- packages/create-sst/bin/presets/standard/nextjs/preset.mjs
+++ packages/create-sst/bin/presets/standard/nextjs/preset.mjs
@@ -2,7 +2,7 @@ import { patch, install, extend, extract, cmd, str_replace } from "create-sst";
 export default [
   extend("presets/base/monorepo"),
   cmd({
-    cmd: "npx create-next-app@latest web --ts --no-eslint --no-src-dir --no-experimental-app --import-alias '@/*' --use-pnpm --tailwind",
+    cmd: "npx create-next-app@latest web --ts --no-eslint --no-src-dir --no-app --no-experimental-app --import-alias '@/*' --use-pnpm --tailwind",
     cwd: "packages",
   }),
   patch({
