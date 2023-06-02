2ecb4269276eaa8f26f5555a89e8d30c3ab709d5
Dax Raad <d@ironbay.co>
2023-06-01 21:51:59 -0400

    function: include handler path in metadata

--- packages/sst/src/constructs/Function.ts
+++ packages/sst/src/constructs/Function.ts
@@ -902,6 +902,7 @@ export class Function extends CDKFunction implements SSTConstruct {
       type: "Function" as const,
       data: {
         arn: this.functionArn,
+        handler: this.props.handler,
         localId: this.node.addr,
         secrets: this.allBindings
           .filter((c) => c instanceof Secret)
