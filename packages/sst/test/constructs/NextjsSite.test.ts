ae633b1bd220f1c318ea03a9cea83bb05085d6df
Guillaume Duboc <guillaumed@theodo.fr>
2023-06-26 18:30:15 +0200

    feat: add test on factory method extension

--- packages/sst/test/constructs/NextjsSite.test.ts
+++ packages/sst/test/constructs/NextjsSite.test.ts
@@ -16,6 +16,10 @@ import {
 import { Vpc } from "aws-cdk-lib/aws-ec2";
 import * as cf from "aws-cdk-lib/aws-cloudfront";
 import { Stack, NextjsSite, NextjsSiteProps } from "../../dist/constructs";
+import { Queue } from "aws-cdk-lib/aws-sqs";
+import { SsrFunction } from "../../dist/constructs/SsrFunction.js";
+import { Function as CdkFunction} from "aws-cdk-lib/aws-lambda";
+import { Distribution } from "../../dist/constructs/Distribution.js";
 
 const sitePath = "test/constructs/nextjs-site";
 
@@ -209,4 +213,44 @@ test("buildCloudWatchRouteName", async () => {
   expect(NextjsSite._test.buildCloudWatchRouteName("/api/[id]")).toEqual(
     "/api/id"
   );
+});
+
+/////////////////////////////
+// Test extending ()
+/////////////////////////////
+
+test("constructor: extending factory methods", async () => {
+  const mockCreateQueue = vi.fn((scope, id, props) => new Queue(scope, id, props));
+  const mockCreateDistribution = vi.fn((scope, id, props) => new Distribution(scope, id, props));
+  const mockCreateSsrFunction = vi.fn((scope, id, props) => new SsrFunction(scope, id, props));
+  const mockCreateFunction = vi.fn((scope, id, props) => new CdkFunction(scope, id, props));
+  
+  class MyNextjsSite extends NextjsSite {
+    protected createQueue(id, props) {
+      return mockCreateQueue(this, id, props);
+    }
+    protected createDistribution(id, props) {
+      return mockCreateDistribution(this, id, props);
+    }
+    protected createSsrFunction(id, props) {
+      return mockCreateSsrFunction(this, id, props);
+    }
+    protected createFunction(id, props) {
+      return mockCreateFunction(this, id, props);
+    }
+  }
+
+  const app = await createApp()
+  const stack = new Stack(app, "stack");
+  new MyNextjsSite(stack, "Site", {
+    path: sitePath,
+    buildCommand: "echo 'skip'",
+  });
+  await app.finish();
+  countResources(stack, "AWS::SQS::Queue", 1);
+  expect(mockCreateQueue).toHaveBeenCalledOnce();
+  countResources(stack, "AWS::CloudFront::Distribution", 1);
+  expect(mockCreateDistribution).toHaveBeenCalledOnce();
+  expect(mockCreateSsrFunction).toHaveBeenCalledOnce();
+  expect(mockCreateFunction).toHaveBeenCalled();
 });
