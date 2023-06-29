e5fc4de81b640be37d76862f52aa54d9259fee0b
Frank <wangfanjie@gmail.com>
2023-06-29 17:36:18 -0400

    NextjsSite: update to OpenNext 2.0.3

--- packages/sst/src/constructs/NextjsSite.ts
+++ packages/sst/src/constructs/NextjsSite.ts
@@ -85,7 +85,7 @@ export class NextjsSite extends SsrSite {
 
   constructor(scope: Construct, id: string, props?: NextjsSiteProps) {
     super(scope, id, {
-      buildCommand: "npx --yes open-next@2.0.0 build",
+      buildCommand: "npx --yes open-next@2.0.3 build",
       ...props,
     });
 
