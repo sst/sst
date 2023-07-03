93c457cd1617328d26c752a6279ce35a5d709738
Eloi Alain <eloia@theodo.fr>
2023-07-03 15:37:08 +0200

    feat: activate auth AWS_IAM on site server function

--- packages/sst/src/constructs/SsrSite.ts
+++ packages/sst/src/constructs/SsrSite.ts
@@ -1045,7 +1045,7 @@ function handler(event) {
     const cfDistributionProps = cdk?.distribution || {};
 
     const fnUrl = this.serverLambdaForRegional!.addFunctionUrl({
-      authType: FunctionUrlAuthType.NONE,
+      authType: FunctionUrlAuthType.AWS_IAM,
     });
 
     return {
