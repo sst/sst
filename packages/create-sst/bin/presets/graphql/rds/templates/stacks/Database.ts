e4a3edfe06af5874824f2e7d55703b1ba77d1cd9
Christine Panus <christine.panus@panusventures.com>
2023-07-02 23:47:34 -0500

    Packages - Best Practice Updates
    * Update main completion prompt to use pnpm as primary example
    * Update Database stack to create money saving non-prod config by default

--- packages/create-sst/bin/presets/graphql/rds/templates/stacks/Database.ts
+++ packages/create-sst/bin/presets/graphql/rds/templates/stacks/Database.ts
@@ -1,11 +1,26 @@
-import { RDS, StackContext } from "sst/constructs";
+// https://docs.sst.dev/constructs/RDS
 
-export function Database({ stack }: StackContext) {
+import { RDS, RDSProps, StackContext } from "sst/constructs";
+
+export function Database({ stack, app }: StackContext) {
+
+  const prodConfig: RDSProps["scaling"] = {
+    autoPause: false,
+    minCapacity: "ACU_8",
+    maxCapacity: "ACU_64",
+  };
+  const devConfig: RDSProps["scaling"] = {
+    autoPause: true,
+    minCapacity: "ACU_2",
+    maxCapacity: "ACU_2",
+  };
+
   const rds = new RDS(stack, "db", {
     engine: "postgresql11.13",
     defaultDatabaseName: "main",
     migrations: "packages/core/migrations",
     types: "packages/core/src/sql.generated.ts",
+    scaling: app.stage === "prod" ? prodConfig : devConfig,
   });
 
   return rds;
