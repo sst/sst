b1f145ddf4f8b86c0c22fd2dc995bc5e73d7850f
Frank <wangfanjie@gmail.com>
2023-07-04 21:13:51 -0400

    Sync

--- packages/sst/src/cli/commands/dev.tsx
+++ packages/sst/src/cli/commands/dev.tsx
@@ -99,9 +99,23 @@ export const dev = (program: Program) =>
         });
 
         bus.subscribe("worker.stdout", async (evt) => {
+          const info = useFunctions().fromID(evt.properties.functionID);
           prefix(evt.properties.requestID);
           const { started } = pending.get(evt.properties.requestID)!;
           for (let line of evt.properties.message.split("\n")) {
+            // Remove prefix from container logs
+            if (info?.runtime === "container") {
+              // handle Node.js container logs
+              // ie. 2023-07-05T00:13:42.448Z\td7330533-2429-4871-a632-ed29a1d32246\tINFO\tfoo!
+              const parts = line.split("\t");
+              if (
+                parts.length >= 4 &&
+                Date.parse(parts[0]) &&
+                parts[1].length === 36
+              ) {
+                line = parts.slice(3).join("\t");
+              }
+            }
             Colors.line(
               prefix(evt.properties.requestID),
               Colors.dim(("+" + (Date.now() - started) + "ms").padEnd(7)),
