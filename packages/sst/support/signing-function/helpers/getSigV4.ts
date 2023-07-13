f832c08879ba1cf72430b04bd11b2310d2472423
Eloi Alain <eloia@theodo.fr>
2023-07-13 18:30:44 +0200

    feat: support long lived credentials (undefined AWS_SESSION_TOKEN)

--- packages/sst/support/signing-function/helpers/getSigV4.ts
+++ packages/sst/support/signing-function/helpers/getSigV4.ts
@@ -2,12 +2,14 @@ import { SignatureV4 } from "@smithy/signature-v4";
 import { Sha256 } from "@aws-crypto/sha256-js";
 
 export const getSigV4 = (region: string) => {
+
+  if (process.env.AWS_ACCESS_KEY_ID === undefined) throw new Error("AWS_ACCESS_KEY_ID missing");
+  if (process.env.AWS_SECRET_ACCESS_KEY === undefined) throw new Error("AWS_SECRET_ACCESS_KEY missing");
+
   const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
   const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
   const sessionToken = process.env.AWS_SESSION_TOKEN;
-  if (!accessKeyId) throw new Error("AWS_ACCESS_KEY_ID missing");
-  if (!secretAccessKey) throw new Error("AWS_SECRET_ACCESS_KEY missing");
-  if (!sessionToken) throw new Error("AWS_SESSION_TOKEN missing");
+
   return new SignatureV4({
     service: "lambda",
     region,
