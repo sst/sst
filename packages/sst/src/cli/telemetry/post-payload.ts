874ca3e352a596680ee3ef5bbe9e4c6132f6a188
Klaus Fleck <klaus.fleck@thetradedesk.com>
2023-07-22 01:09:10 +0100

    catch telemetry DNS error

--- packages/sst/src/cli/telemetry/post-payload.ts
+++ packages/sst/src/cli/telemetry/post-payload.ts
@@ -1,23 +1,27 @@
-import https from "https";
+import https from 'https';
 
 export function postPayload(endpoint: string, body: any) {
   return new Promise<void>((resolve, reject) => {
     try {
-      const req = https.request(
-        endpoint,
-        {
-          method: "POST",
-          headers: { "content-type": "application/json" },
-          timeout: 5000,
-        },
-        (resp) => {
-          if (resp.statusCode !== 200) {
-            reject(new Error(`Unexpected status code: ${resp.statusCode}`));
-            return;
+      const req = https
+        .request(
+          endpoint,
+          {
+            method: 'POST',
+            headers: { 'content-type': 'application/json' },
+            timeout: 5000
+          },
+          (resp) => {
+            if (resp.statusCode !== 200) {
+              reject(new Error(`Unexpected status code: ${resp.statusCode}`));
+              return;
+            }
+            resolve();
           }
-          resolve();
-        }
-      );
+        )
+        .on('error', () => {
+          // catches connect ECONNREFUSED while the below catch does not work as expected
+        });
       req.write(JSON.stringify(body));
       req.end();
     } catch (ex) {
