b0dd52c2d4a609683d2701ee1a88a1bd5a003de9
Dax Raad <d@ironbay.co>
2023-08-07 17:13:59 -0400

    rds: allow migrating from CLI

---
+++ .changeset/sour-weeks-glow.md
@@ -0,0 +1,5 @@
+---
+"create-sst": patch
+---
+
+rds: allow migrating from CLI
--- packages/create-sst/bin/presets/graphql/rds/preset.mjs
+++ packages/create-sst/bin/presets/graphql/rds/preset.mjs
@@ -11,12 +11,27 @@ export default [
     operations: [{ op: "add", path: "/scripts/gen", value: "hygen" }],
   }),
   install({
+    packages: ["tsx"],
+    path: "packages/core",
+    dev: true,
+  }),
+  install({
     packages: [
-      "kysely",
+      "kysely@0.25.0",
       "kysely-data-api",
       "aws-sdk",
       "@aws-sdk/client-rds-data",
     ],
     path: "packages/core",
+  }),
+  patch({
+    file: "./packages/core/package.json",
+    operations: [
+      {
+        op: "add",
+        path: "/scripts/migrate",
+        value: "sst bind tsx ./src/migrator.ts",
+      },
+    ],
   }),
 ];
