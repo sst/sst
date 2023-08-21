a09c720704573fa6d913ca12acd0867fc35149ac
Omar Abdelkader <omikader@gmail.com>
2023-08-21 14:06:22 -0700

    Service: Add node client and docs (#3246)
    * Service: Add node client and docs
    
    * Add changeset
    
    * Sync
    
    ---------
    
    Co-authored-by: Frank <wangfanjie@gmail.com>

--- www/sidebars.js
+++ www/sidebars.js
@@ -235,6 +235,7 @@ module.exports = {
         "clients/config",
         "clients/queue",
         "clients/bucket",
+        "clients/service",
         "clients/graphql",
         "clients/function",
         "clients/event-bus",
