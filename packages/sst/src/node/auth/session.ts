4660a0f76df006c62b79ab565e9d8035a562a7b4
Dax Raad <d@ironbay.co>
2023-09-19 15:32:41 -0400

    update context

--- packages/sst/src/node/auth/session.ts
+++ packages/sst/src/node/auth/session.ts
@@ -1,6 +1,6 @@
 import { createSigner, createVerifier, SignerOptions } from "fast-jwt";
 import { APIGatewayProxyStructuredResultV2 } from "aws-lambda";
-import { Context } from "../../context/context.js";
+import { Context } from "../../context/context2.js";
 import { useCookie, useHeader } from "../api/index.js";
 import { getPrivateKey, getPublicKey } from "./auth.js";
 import { useContextType } from "../../context/handler.js";
