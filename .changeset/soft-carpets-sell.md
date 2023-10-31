2e2d425dc29cb78ebab63cf31c00ec00af0ddec2
Michael McDermott <michael@dinewise.com>
2023-10-31 17:42:41 -0400

    fix: add delete parameters permissions to AuthKeys role (#3417)
    * fix: add delete parameters permissions to authkeys role
    
    * Create soft-carpets-sell.md
    
    ---------
    
    Co-authored-by: Dax <mail@thdxr.com>

---
+++ .changeset/soft-carpets-sell.md
@@ -0,0 +1,5 @@
+---
+"sst": patch
+---
+
+fix: add delete parameters permissions to AuthKeys role
--- packages/sst/src/constructs/future/Auth.ts
+++ packages/sst/src/constructs/future/Auth.ts
@@ -147,6 +147,7 @@ export class Auth extends Construct implements SSTConstruct {
             "ssm:GetParameter",
             "ssm:PutParameter",
             "ssm:DeleteParameter",
+            "ssm:DeleteParameters",
           ],
           resources: [
             `arn:${stack.partition}:ssm:${stack.region}:${stack.account}:parameter/*`,
