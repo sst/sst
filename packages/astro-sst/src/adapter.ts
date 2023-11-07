21127596eb99cf1af5721673724bc9a5c5c663da
Zach Cardoza <2280384+bayssmekanique@users.noreply.github.com>
2023-11-07 07:15:37 -0800

    fix: remove config destructuring FIxes bug in MDX plugin

--- packages/astro-sst/src/adapter.ts
+++ packages/astro-sst/src/adapter.ts
@@ -106,9 +106,7 @@ export default function createIntegration(
         // Enable sourcemaps
         updateConfig({
           vite: {
-            ...config.vite,
             build: {
-              ...config.vite?.build,
               sourcemap: true,
             },
           },
