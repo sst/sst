b4444afe3a6306bc985dbe5adcd7fc892616d39b
Dax Raad <d@ironbay.co>
2023-11-29 12:27:03 -0500

    push aws helper

--- internal/components/src/components/helpers/aws.ts
+++ internal/components/src/components/helpers/aws.ts
@@ -1,4 +1,12 @@
+import crypto from "crypto";
 const bootstrapBuckets: Record<string, Promise<string>> = {};
+import {
+  SSMClient,
+  GetParameterCommand,
+  ParameterNotFound,
+  PutParameterCommand,
+} from "@aws-sdk/client-ssm";
+import { S3Client, CreateBucketCommand } from "@aws-sdk/client-s3";
 
 export const AWS = {
   bootstrap: {
@@ -7,11 +15,48 @@ export const AWS = {
         return bootstrapBuckets[region]!;
       }
 
-      const bucket = new Promise<string>((resolve, reject) => {
-        // do some stuff
+      const ssm = new SSMClient({
+        region,
       });
+      const s3 = new S3Client({
+        region,
+      });
+      try {
+        const bucket = (async () => {
+          const result = await ssm
+            .send(
+              new GetParameterCommand({
+                Name: `/sst/bootstrap`,
+              }),
+            )
+            .catch((err) => {
+              if (err instanceof ParameterNotFound) return;
+              throw err;
+            });
 
-      return (bootstrapBuckets[region] = bucket);
+          if (result?.Parameter?.Value) return result.Parameter.Value;
+
+          const name = `sst-bootstrap-${crypto.randomUUID()}`;
+          await s3.send(
+            new CreateBucketCommand({
+              Bucket: name,
+            }),
+          );
+          await ssm.send(
+            new PutParameterCommand({
+              Name: `/sst/bootstrap`,
+              Value: name,
+              Type: "String",
+            }),
+          );
+          return name;
+        })();
+
+        return (bootstrapBuckets[region] = bucket);
+      } finally {
+        s3.destroy();
+        ssm.destroy();
+      }
     },
   },
 };
