d5921dd1081c3d796a9dce8f79abe3b9fb3ce91c
Zach Cardoza <2280384+bayssmekanique@users.noreply.github.com>
2023-12-07 08:37:39 -0800

    chore: fix linting issue

--- packages/sst/src/constructs/AstroSite.ts
+++ packages/sst/src/constructs/AstroSite.ts
@@ -46,7 +46,7 @@ export class AstroSite extends SsrSite {
   /**
    * The purpose of the `getCFRoutingFunction` method is to generate a CloudFront function that mimics functionality often offered by
    * full featured web servers. This function will perform redirects and rewrites based on the routes defined by the Astro build.
-   * 
+   *
    * This has been optimized as much as the current implementation allows. The next step in optimization would be to break
    * the routes list into a btree based on the route pattern. This would allow for a much faster lookup of the route that matches.
    */
@@ -77,15 +77,15 @@ export class AstroSite extends SsrSite {
             }}`;
           }
         })
-        .filter(compressedRoute => compressedRoute)
+        .filter((compressedRoute) => compressedRoute)
         .join(",") +
       "]";
 
     return `var x = ${serializedRoutes}.find((y)=>y.p.test(request.uri));if(x){if(x.t===1){var w=x.h;x.p.exec(request.uri).forEach((k,l)=>{w=w.replace(\`\\\${\${l}}\`,k);});return {statusCode:x.s||308,headers:{location:{value:w}},};}else if(x.t===0&&x.r){${
-        pageResolution === "file"
-          ? `request.uri=request.uri==="/"?"/index.html":request.uri.replace(/\\/?$/,".html");`
-          : `request.uri=request.uri.replace(/\\/?$/,"/index.html");`
-      }}}`;
+      pageResolution === "file"
+        ? `request.uri=request.uri==="/"?"/index.html":request.uri.replace(/\\/?$/,".html");`
+        : `request.uri=request.uri.replace(/\\/?$/,"/index.html");`
+    }}}`;
   }
 
   protected plan() {
