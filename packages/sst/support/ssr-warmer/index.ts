7e717930d83ff3efc885c147ce50ae69ccc07e2e
Dorseuil Nicolas <nicodorseuil@yahoo.fr>
2023-12-11 11:43:13 +0100

    support for warmer with splittedFn

--- packages/sst/support/ssr-warmer/index.ts
+++ packages/sst/support/ssr-warmer/index.ts
@@ -2,8 +2,7 @@ import { LambdaClient, InvokeCommand } from "@aws-sdk/client-lambda";
 import type { Context } from "aws-lambda";
 
 const lambda = new LambdaClient({});
-const FUNCTION_NAME = process.env.FUNCTION_NAME!;
-const CONCURRENCY = parseInt(process.env.CONCURRENCY!);
+const warmParams = JSON.parse(process.env.WARM_PARAMS!) as { concurrency: number, function: string }[];
 
 function generateUniqueId() {
   return Math.random().toString(36).slice(2, 8);
@@ -23,58 +22,65 @@ export interface WarmerResponse {
 
 export async function handler(_event: any, context: Context) {
   const warmerId = `warmer-${generateUniqueId()}`;
-  console.log({
-    event: "warmer invoked",
-    functionName: FUNCTION_NAME,
-    concurrency: CONCURRENCY,
-    warmerId,
-  });
 
-  // Warm
-  const ret = await Promise.all(
-    Array.from({ length: CONCURRENCY }, (_v, i) => i).map((i) => {
-      try {
-        return lambda.send(
-          new InvokeCommand({
-            FunctionName: FUNCTION_NAME,
-            InvocationType: "RequestResponse",
-            Payload: JSON.stringify({
-              type: "warmer",
-              warmerId,
-              index: i,
-              concurrency: CONCURRENCY,
-              delay: 75,
-            } satisfies WarmerEvent),
-          })
-        );
-      } catch (e) {
-        console.error(`failed to warm up #${i}`, e);
-        // ignore error
+  for (const warmParam of warmParams) {
+    const { concurrency: CONCURRENCY, function: FUNCTION_NAME } = warmParam;
+
+
+    console.log({
+      event: "warmer invoked",
+      functionName: FUNCTION_NAME,
+      concurrency: CONCURRENCY,
+      warmerId,
+    });
+
+    // Warm
+    const ret = await Promise.all(
+      Array.from({ length: CONCURRENCY }, (_v, i) => i).map((i) => {
+        try {
+          return lambda.send(
+            new InvokeCommand({
+              FunctionName: FUNCTION_NAME,
+              InvocationType: "RequestResponse",
+              Payload: JSON.stringify({
+                type: "warmer",
+                warmerId,
+                index: i,
+                concurrency: CONCURRENCY,
+                delay: 75,
+              } satisfies WarmerEvent),
+            })
+          );
+        } catch (e) {
+          console.error(`failed to warm up #${i}`, e);
+          // ignore error
+        }
+      })
+    );
+
+    // Print status
+    const warmedServerIds: string[] = [];
+    ret.forEach((r, i) => {
+      if (r?.StatusCode !== 200 || !r?.Payload) {
+        console.error(`failed to warm up #${i}:`, r?.Payload?.toString());
+        return;
       }
-    })
-  );
-
-  // Print status
-  const warmedServerIds: string[] = [];
-  ret.forEach((r, i) => {
-    if (r?.StatusCode !== 200 || !r?.Payload) {
-      console.error(`failed to warm up #${i}:`, r?.Payload?.toString());
-      return;
-    }
-    const payloadString = r.Payload.transformToString();
-    if (payloadString) {
-      const payload = JSON.parse(
-        r.Payload.transformToString()
-      ) as WarmerResponse;
-      warmedServerIds.push(payload.serverId);
-    } else {
-      warmedServerIds.push("unknown");
-    }
-  });
-  console.log({
-    event: "warmer result",
-    sent: CONCURRENCY,
-    success: warmedServerIds.length,
-    uniqueServersWarmed: [...new Set(warmedServerIds)].length,
-  });
+      const payloadString = r.Payload.transformToString();
+      if (payloadString) {
+        const payload = JSON.parse(
+          r.Payload.transformToString()
+        ) as WarmerResponse;
+        warmedServerIds.push(payload.serverId);
+      } else {
+        warmedServerIds.push("unknown");
+      }
+    });
+    console.log({
+      event: "warmer result",
+      sent: CONCURRENCY,
+      name: FUNCTION_NAME,
+      success: warmedServerIds.length,
+      uniqueServersWarmed: [...new Set(warmedServerIds)].length,
+    });
+  }
 }
