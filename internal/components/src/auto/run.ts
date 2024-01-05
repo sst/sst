0937789b929afd64cb02a7ce20bf256dbbc8cda2
Frank <wangfanjie@gmail.com>
2024-01-05 11:09:18 -0500

    Fix AWS provider having invalid names

--- internal/components/src/auto/run.ts
+++ internal/components/src/auto/run.ts
@@ -20,12 +20,15 @@ export async function run(program: PulumiFn) {
         };
       }
       return undefined;
-    },
+    }
   );
   runtime.registerStackTransformation(
     (args: util.ResourceTransformationArgs) => {
       let normalizedName = args.name;
-      if (args.type === "pulumi-nodejs:dynamic:Resource") {
+      if (
+        args.type === "pulumi-nodejs:dynamic:Resource" ||
+        args.type === "pulumi:providers:aws"
+      ) {
         const parts = args.name.split(".");
         if (parts.length === 3 && parts[1] === "sst") {
           normalizedName = parts[0];
@@ -34,12 +37,12 @@ export async function run(program: PulumiFn) {
 
       if (!normalizedName.match(/^[A-Z][a-zA-Z0-9]*$/)) {
         throw new Error(
-          `Invalid component name "${normalizedName}". Component names must start with an uppercase letter and contain only alphanumeric characters.`,
+          `Invalid component name "${normalizedName}". Component names must start with an uppercase letter and contain only alphanumeric characters.`
         );
       }
 
       return undefined;
-    },
+    }
   );
 
   const results = await program();
