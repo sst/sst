ced776e9d50d34c4696dd40553c3ad46e3d853ad
Jay V <air@live.ca>
2024-02-08 18:15:17 -0500

    Lander OG

--- www/config.ts
+++ www/config.ts
@@ -4,4 +4,5 @@ export default {
   discord: "https://sst.dev/discord",
   twitter: "https://x.com/SST_dev",
   socialCard: "https://social-cards.sst.dev",
+  description: "Modern full-stack apps in your infrastructure",
 };
