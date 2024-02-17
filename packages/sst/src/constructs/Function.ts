28aa3895a3fe26ee1e1cff619f81b0a951026bb7
Michael Aldape <michael.aldape@flosports.tv>
2024-02-16 21:43:56 -0600

    add trim

--- packages/sst/src/constructs/Function.ts
+++ packages/sst/src/constructs/Function.ts
@@ -912,7 +912,7 @@ export class Function extends CDKFunction implements SSTConstruct {
       }
 
       // Lambda descriptions have a limit of 256 chars. Subtract 26 chars to account for Live Lambda label suffix.
-      const liveLambdaDescription = `${props.description ? props.description.substring(0, 230) : ''} (SST Live Lambda handler)`
+      const liveLambdaDescription = `${props.description ? props.description.substring(0, 230) : ''} (SST Live Lambda handler)`.trim()
 
       super(scope, id, {
         ...props,
