00ffffd9e918637f973b27fff830c3afe78c3395
Frank <frank@sst.dev>
2024-03-01 14:48:04 -0500

    Sync

--- examples/playground/sst.config.ts
+++ examples/playground/sst.config.ts
@@ -1,23 +1,19 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
 export default $config({
-  app() {
+  app(input) {
     return {
       name: "playground",
+      removalPolicy: input?.stage === "production" ? "retain" : "remove",
       providers: {
-        aws: {
-          region: "us-east-1",
-        },
+        aws: {},
         cloudflare: {
           accountId: "24beb0945bae6b37c2b147db108c6ec8",
         },
       },
-      removalPolicy: "remove",
     };
   },
   async run() {
-    //return runVectorExample();
-
     const bucket = new sst.aws.Bucket("MyBucket", {
       public: true,
       transform: {
@@ -27,53 +23,15 @@ export default $config({
       },
     });
 
-    const app = new sst.aws.Function("MyApp", {
-      handler: "functions/handler-example/index.handler",
-      link: [bucket],
-      url: true,
-    });
+    //    const app = new sst.aws.Function("MyApp", {
+    //      handler: "functions/handler-example/index.handler",
+    //      link: [bucket],
+    //      url: true,
+    //    });
 
     return {
       bucket: bucket.name,
-      app: app.url,
+      //      app: app.url,
     };
   },
 });
-
-function runVectorExample() {
-  const vector = new sst.aws.Vector("MyVectorDB", {
-    model: "text-embedding-ada-002",
-    //model: "amazon.titan-embed-image-v1",
-    openAiApiKey: new sst.Secret("OpenAiApiKey").value,
-  });
-
-  const seeder = new sst.aws.Function("Seeder", {
-    handler: "functions/vector-example/index.seeder",
-    link: [vector],
-    copyFiles: [
-      { from: "functions/vector-example/iron-man.jpg", to: "iron-man.jpg" },
-      {
-        from: "functions/vector-example/black-widow.jpg",
-        to: "black-widow.jpg",
-      },
-      {
-        from: "functions/vector-example/spider-man.jpg",
-        to: "spider-man.jpg",
-      },
-      { from: "functions/vector-example/thor.jpg", to: "thor.jpg" },
-      {
-        from: "functions/vector-example/captain-america.jpg",
-        to: "captain-america.jpg",
-      },
-    ],
-    url: true,
-  });
-
-  const app = new sst.aws.Function("MyApp", {
-    handler: "functions/vector-example/index.app",
-    link: [vector],
-    url: true,
-  });
-
-  return { seeder: seeder.url, app: app.url };
-}
