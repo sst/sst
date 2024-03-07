fc7766275378a24cac884270297a73c7f771e0ef
Frank <frank@sst.dev>
2024-03-07 10:55:04 -0500

    Docs wip

--- pkg/platform/src/global.d.ts
+++ pkg/platform/src/global.d.ts
@@ -60,12 +60,12 @@ declare global {
    * declare value1: Output<string>;
    * declare value2: Output<string>;
    *
-   * $all([value1, value2]).apply(([value1, value2]) =>
+   * $resolve([value1, value2]).apply(([value1, value2]) =>
    *   console.log({ value1, value2 })
    * );
    * ```
    */
-  export const $all: typeof util.all;
+  export const $resolve: typeof util.all;
   /**
    * String interpolation with Output values.
    *
@@ -82,6 +82,58 @@ declare global {
    * ```
    */
   export const $interpolate: typeof util.interpolate;
+  /**
+   * Takes a sequence of Output values or plain JavaScript values, stringifies each,
+   * and concatenates all values into one final string.
+   *
+   * @example
+   * ```ts
+   * const bucket = new sst.aws.Bucket("MyBucket");
+   * const description = $concat("This is a bucket named ", bucket.name);
+   * ```
+   *
+   * This is equivalent to
+   * ```ts
+   * const description = bucket.name.apply(name =>
+   *   "This is a bucket named ".concat(name)
+   * );
+   * ```
+   */
+  export const $concat: typeof util.concat;
+  /**
+   * Takes an Output value or plain Javascript value, use JSON.parse to deserialize
+   * the resolved JSON string into a JSON object.
+   *
+   * @example
+   * ```ts
+   * const policy = $jsonParse(policyStr);
+   * ```
+   *
+   * This is equivalent to
+   * ```ts
+   * const policy = policyStr.apply((policy) =>
+   *   JSON.parse(policy)
+   * );
+   * ```
+   */
+  export const $jsonParse: typeof util.jsonParse;
+  /**
+   * Takes an Output value or plain JSON object, use JSON.stringify to serialize
+   * the resolved JSON object into a JSON string.
+   *
+   * @example
+   * ```ts
+   * const policy = $jsonStringify(policyObj);
+   * ```
+   *
+   * This is equivalent to
+   * ```ts
+   * const policy = policyObj.apply((policy) =>
+   *   JSON.stringify(policy)
+   * );
+   * ```
+   */
+  export const $jsonStringify: typeof util.jsonStringify;
 
   /**
    * Information about the app
--- pkg/platform/src/shim/run.js
+++ pkg/platform/src/shim/run.js
@@ -4,15 +4,19 @@ import { Link } from "../components/link";
 import { $config } from "../config";
 
 const $secrets = JSON.parse(process.env.SST_SECRETS || "{}");
-const { output, apply, all, interpolate } = util;
+const { output, apply, all, interpolate, concat, jsonParse, jsonStringify } =
+  util;
 
 const makeLinkable = Link.makeLinkable;
 export {
   makeLinkable as "$linkable",
   output as "$output",
   apply as "$apply",
-  all as "$all",
+  all as "$resolve",
   interpolate as "$interpolate",
+  concat as "$concat",
+  jsonParse as "$jsonParse",
+  jsonStringify as "$jsonStringify",
   util as "$util",
   sst as "sst",
   $config as "$config",
