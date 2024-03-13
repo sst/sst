4b090649e1a4f41bd138d91858e6edc189822c0d
Jay V <air@live.ca>
2024-03-13 16:27:35 -0400

    Docs config

--- www/sst.config.ts
+++ www/sst.config.ts
@@ -5,6 +5,7 @@ export default $config({
     return {
       name: "www",
       removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
       providers: {
         aws: {
           profile: (() => {
