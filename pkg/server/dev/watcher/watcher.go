6ed483f832d95a437143f2c1e02be2490ea80e9e
Dax Raad <d@ironbay.co>
2024-03-26 11:14:46 -0400

    better logging for file watcher

--- pkg/server/dev/watcher/watcher.go
+++ pkg/server/dev/watcher/watcher.go
@@ -53,10 +53,10 @@ func Start(ctx context.Context, root string) (util.CleanupFunc, error) {
 				if !ok {
 					return
 				}
+				slog.Info("file event", "path", event.Name, "op", event.Op)
 				if event.Op&fsnotify.Write != fsnotify.Write {
 					continue
 				}
-				slog.Info("file changed", "path", event.Name)
 				bus.Publish(&FileChangedEvent{Path: event.Name})
 			case <-ctx.Done():
 				return
