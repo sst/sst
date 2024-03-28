6157ec4629c147592fe1c85753c3bb6be53d8b6d
Frank <frank@sst.dev>
2024-03-28 17:04:50 -0400

    Cognito example no one should be using. SST Auth coming soon to Ion.

---
+++ examples/aws-cognito/sst.config.ts
@@ -0,0 +1,78 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "cognito",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
+    };
+  },
+  async run() {
+    const region = aws.getRegionOutput().name;
+
+    const userPool = new aws.cognito.UserPool("MyUserPool", {
+      name: `${$app.name}-${$app.stage}-MyUserPool`,
+      accountRecoverySetting: {
+        recoveryMechanisms: [
+          {
+            name: "verified_phone_number",
+            priority: 1,
+          },
+          {
+            name: "verified_email",
+            priority: 2,
+          },
+        ],
+      },
+      adminCreateUserConfig: {
+        allowAdminCreateUserOnly: false,
+      },
+      autoVerifiedAttributes: ["email"],
+      usernameAttributes: ["email"],
+      usernameConfiguration: {
+        caseSensitive: false,
+      },
+      verificationMessageTemplate: {
+        defaultEmailOption: "CONFIRM_WITH_CODE",
+        emailMessage: "The verification code to your new account is {####}",
+        emailSubject: "Verify your new account",
+        smsMessage: "The verification code to your new account is {####}",
+      },
+    });
+
+    const userPoolClient = new aws.cognito.UserPoolClient("MyClient", {
+      name: `${$app.name}-${$app.stage}-MyClient`,
+      userPoolId: userPool.id,
+      allowedOauthFlows: ["implicit", "code"],
+      allowedOauthFlowsUserPoolClient: true,
+      allowedOauthScopes: [
+        "profile",
+        "phone",
+        "email",
+        "openid",
+        "aws.cognito.signin.user.admin",
+      ],
+      callbackUrls: ["https://example.com"],
+      supportedIdentityProviders: ["COGNITO"],
+    });
+
+    const identityPool = new aws.cognito.IdentityPool("MyIdentityPool", {
+      identityPoolName: `${$app.name}-${$app.stage}-MyIdentityPool`,
+      allowUnauthenticatedIdentities: true,
+      cognitoIdentityProviders: [
+        {
+          clientId: userPoolClient.id,
+          providerName: $interpolate`cognito-idp.${region}.amazonaws.com/${userPool.id}`,
+        },
+      ],
+    });
+
+    return {
+      Region: region,
+      UserPoolId: userPool.id,
+      UserPoolClientId: userPoolClient.id,
+      IdentityPoolId: identityPool.id,
+    };
+  },
+});
