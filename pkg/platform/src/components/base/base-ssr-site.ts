3dcf224b9a2dd881ab422f858671ac7eb21f86a6
Dax Raad <d@ironbay.co>
2024-04-05 17:29:49 -0400

    pass aws creds for build

--- pkg/platform/src/components/base/base-ssr-site.ts
+++ pkg/platform/src/components/base/base-ssr-site.ts
@@ -177,6 +177,10 @@ export function buildApp(
             stdio: "inherit",
             env: {
               ...process.env,
+              AWS_ACCESS_KEY_ID: process.env.SST_AWS_ACCESS_KEY_ID,
+              AWS_SESSION_TOKEN: process.env.SST_AWS_SESSION_TOKEN,
+              AWS_SECRET_ACCESS_KEY: process.env.SST_AWS_SECRET_ACCESS_KEY,
+              AWS_REGOIN: process.env.SST_AWS_REGION,
               ...environment,
               ...linkEnvs,
             },
