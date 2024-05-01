13aba4d4cae7119435d4bcdad2184f6ac79062d6
Bartosz Golebiowski <bartosz.golebiowski24@gmail.com>
2024-05-01 11:53:47 +0200

    fix: Update Remix.run -> remix.config.js
    esm does not work out of the box, so I just changed it to cjs

--- examples/remix-app/packages/frontend/remix.config.js
+++ examples/remix-app/packages/frontend/remix.config.js
@@ -5,7 +5,7 @@ module.exports = {
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
-  serverModuleFormat: "esm",
+  serverModuleFormat: "cjs",
   future: {
     v2_dev: true,
     v2_errorBoundary: true,
