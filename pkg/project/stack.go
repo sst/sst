2be46dd2b0395bc16ef4c328032569587c705f23
Dax Raad <d@ironbay.co>
2024-05-09 22:12:24 -0400

    log stack failures

--- pkg/project/stack.go
+++ pkg/project/stack.go
@@ -511,6 +511,7 @@ func (s *stack) Run(ctx context.Context, input *StackInput) error {
 
 	slog.Info("done running stack command")
 	if err != nil {
+		slog.Error("stack run failed", "error", err)
 		return ErrStackRunFailed
 	}
 	return nil
