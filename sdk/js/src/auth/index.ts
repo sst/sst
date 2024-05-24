d076554046be6c63d8bf4d80622591dd5c633e4a
Dax Raad <d@ironbay.co>
2024-05-23 23:31:41 -0400

    rework auth sdk

--- sdk/js/src/auth/index.ts
+++ sdk/js/src/auth/index.ts
@@ -1,4 +1,12 @@
-export * from "./adapter/index.js";
 export * from "./session.js";
 export * from "./handler.js";
 export { Issuer } from "openid-client";
+
+import { AuthHandler } from "./handler.js";
+import { createSessionBuilder } from "./session.js";
+
+export module auth {
+  export type Issuer = import("openid-client").Issuer;
+  export const authorizer = AuthHandler;
+  export const sessions = createSessionBuilder;
+}
