62c5a69c39890b9cc9731fde5f700d9e286d62ca
Christopher M <cmanou@users.noreply.github.com>
2024-06-20 11:26:21 +1000

    fix: Sharp bundling install issue (#548)

--- pkg/platform/src/runtime/node.ts
+++ pkg/platform/src/runtime/node.ts
@@ -163,7 +163,7 @@ export async function build(
       const cmd = ["npm install"];
       if (installPackages.includes("sharp")) {
         cmd.push(
-          "--platform=linux",
+          "--os=linux --libc=glibc",
           input.architecture === "arm64" ? "--arch=arm64" : "--arch=x64",
         );
       }
