98580780b0d2ce659e1e34fcf6169fcaefef9a44
Dax Raad <d@ironbay.co>
2024-06-24 23:28:29 -0400

    only trigger file watcher on fsnotify Write

--- examples/aws-static-site/sst.config.ts
+++ examples/aws-static-site/sst.config.ts
@@ -9,14 +9,44 @@ export default $config({
   app(input) {
     return {
       name: "aws-static-site",
-      providers: {
-        aws: {},
-      },
+      home: "aws",
       removal: input?.stage === "production" ? "retain" : "remove",
     };
   },
   async run() {
+    const cfFunction = new aws.cloudfront.Function("MySiteFunction", {
+      code: `
+    function handler(event) {
+        var request = event.request;
+        var uri = request.uri;
+        
+        // Check whether the URI is missing a file name.
+        if (uri.endsWith('/')) {
+            request.uri += 'index.html';
+        } 
+        // Check whether the URI is missing a file extension.
+        else if (!uri.includes('.')) {
+            request.uri += '/index.html';
+        }
+
+        return request;
+    }`,
+      runtime: "cloudfront-js-1.0",
+      comment: "A function to add 'index.html' to missing URIs",
+    });
     // Deploys the current directory as a static site
-    new sst.aws.StaticSite("MySite");
+    new sst.aws.StaticSite("MySite", {
+      transform: {
+        cdn: (args) => {
+          // @ts-expect-error
+          args.defaultCacheBehavior.functionAssociations = [
+            {
+              eventType: "viewer-request",
+              functionArn: cfFunction.arn,
+            },
+          ];
+        },
+      },
+    });
   },
 });
---
+++ examples/sst-env.d.ts
@@ -0,0 +1,3 @@
+/* tslint:disable */
+/* eslint-disable */
+/// <reference path="aws-static-site/sst-env.d.ts" />
