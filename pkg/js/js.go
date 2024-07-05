92f90daa1ca57f135da5e03a265d109f93d02b58
Dax Raad <d@ironbay.co>
2024-07-05 14:18:06 -0400

    config: fix issue where sst crashes on certain invalid config build errors

--- pkg/js/js.go
+++ pkg/js/js.go
@@ -59,12 +59,7 @@ const __dirname = topLevelFileUrlToPath(new topLevelURL(".", import.meta.url))
 	})
 	if len(result.Errors) > 0 {
 		for _, err := range result.Errors {
-			slog.Error("esbuild error",
-				"text", err.Text,
-				"location.file", err.Location.File,
-				"location.line", err.Location.Line,
-				"column", err.Location.Column,
-			)
+			slog.Error("esbuild error", "text", err.Text)
 		}
 		return result, fmt.Errorf("%s", FormatError(result.Errors))
 	}
@@ -76,6 +71,10 @@ const __dirname = topLevelFileUrlToPath(new topLevelURL(".", import.meta.url))
 func FormatError(input []esbuild.Message) string {
 	lines := []string{}
 	for _, err := range input {
+		if err.Location == nil {
+			lines = append(lines, fmt.Sprintf("%v", err.Text))
+			continue
+		}
 		lines = append(lines, fmt.Sprintf("%v:%v:%v: %v", err.Location.File, err.Location.Line, err.Location.Column, err.Text))
 	}
 	return strings.Join(lines, "\n")
