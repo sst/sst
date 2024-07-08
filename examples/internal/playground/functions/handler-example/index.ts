8ea5b69fc11208e07832760985ff1ea8c9d29d32
Frank <frank@sst.dev>
2024-07-09 07:07:43 +0800

    sync example

--- examples/internal/playground/functions/handler-example/index.ts
+++ examples/internal/playground/functions/handler-example/index.ts
@@ -3,6 +3,6 @@ import { Resource } from "sst";
 export const handler = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ event, resources: Resource.MyBucket }, null, 2),
+    body: JSON.stringify({ event, resources: Resource.MyBucket }, null, 4),
   };
 };
