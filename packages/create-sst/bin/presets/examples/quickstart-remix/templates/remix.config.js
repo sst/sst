5aaedac8e67042c12acbf7f37ffbd5df4c4ffb6d
lukas <lukas.jsk@gmail.com>
2024-07-08 09:34:06 +0200

    Updated template to use Vite.

--- packages/create-sst/bin/presets/examples/quickstart-remix/templates/remix.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('@remix-run/dev').AppConfig} */
-export default {
-  ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
-  serverModuleFormat: "esm",
-};
