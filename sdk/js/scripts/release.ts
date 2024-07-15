af965906ffcebe8a91a7415cd3b932e73993c56b
Dax Raad <d@ironbay.co>
2024-07-15 12:23:25 -0400

    sync

--- sdk/js/scripts/release.ts
+++ sdk/js/scripts/release.ts
@@ -3,7 +3,8 @@
 import { $ } from "bun";
 
 // check if git is dirty
-const status = await $`git status --porcelain > /dev/null`;
+const status = await $`git status --porcelain`;
+console.log(status.stdout.toString().trim());
 if (status.stdout) {
   console.error("git status is dirty");
   process.exit(1);
