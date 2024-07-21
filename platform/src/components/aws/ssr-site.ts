b3a9dcc5cfe05095a25f158c63bf4aa00ad59b24
Clemens Stolle <klaemo@fastmail.fm>
2024-07-21 16:14:47 +0200

    ssr-site: apply custom content type (#722)

--- platform/src/components/aws/ssr-site.ts
+++ platform/src/components/aws/ssr-site.ts
@@ -393,7 +393,7 @@ export function createServersAndDistribution(
                       key: path.posix.join(copy.to, file),
                       hash,
                       cacheControl: fileOption.cacheControl,
-                      contentType: getContentType(file, "UTF-8"),
+                      contentType: fileOption.contentType ?? getContentType(file, "UTF-8"),
                     };
                   }),
                 )),
