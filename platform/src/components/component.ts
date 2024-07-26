bc2d48b2922c63cab49869aa1f22d4e8391151a0
Frank <frank@sst.dev>
2024-07-26 13:52:45 -0400

    Respect resource names

--- platform/src/components/component.ts
+++ platform/src/components/component.ts
@@ -57,51 +57,124 @@ export class Component extends ComponentResource {
             !args.name.startsWith(args.opts.parent!.__name)
           ) {
             throw new Error(
-              `In "${name}" component, the name of "${args.name}" (${args.type}) is not prefixed with parent's name`,
+              `In "${name}" component, the logical name of "${args.name}" (${args.type}) is not prefixed with parent's name`,
             );
           }
 
           // Ensure physical names are prefixed with app/stage
-          if (args.type.startsWith("sst:")) return undefined;
-          if (args.type === "pulumi-nodejs:dynamic:Resource") return undefined;
-          if (args.type === "random:index/randomId:RandomId") return undefined;
+          // note: We are setting the default names here instead of inline when creating
+          //       the resource is b/c the physical name is inferred from the logical name.
+          //       And it's convenient to access the logical name here.
+          if (
+            args.type.startsWith("sst:") ||
+            args.type === "pulumi-nodejs:dynamic:Resource" ||
+            args.type === "random:index/randomId:RandomId" ||
+            // resources manually named
+            [
+              "aws:appsync/dataSource:DataSource",
+              "aws:appsync/function:Function",
+              "aws:appsync/resolver:Resolver",
+              "aws:cognito/identityPool:IdentityPool",
+              "aws:ecs/service:Service",
+              "aws:ecs/taskDefinition:TaskDefinition",
+              "aws:lb/targetGroup:TargetGroup",
+              "aws:s3/bucketV2:BucketV2",
+              "aws:cloudwatch/eventBus:EventBus",
+            ].includes(args.type) ||
+            // resources not prefixed
+            [
+              "aws:acm/certificate:Certificate",
+              "aws:acm/certificateValidation:CertificateValidation",
+              "aws:apigateway/deployment:Deployment",
+              "aws:apigateway/integration:Integration",
+              "aws:apigateway/method:Method",
+              "aws:apigateway/resource:Resource",
+              "aws:apigateway/stage:Stage",
+              "aws:apigatewayv2/apiMapping:ApiMapping",
+              "aws:apigatewayv2/domainName:DomainName",
+              "aws:apigatewayv2/integration:Integration",
+              "aws:apigatewayv2/route:Route",
+              "aws:apigatewayv2/stage:Stage",
+              "aws:appautoscaling/target:Target",
+              "aws:appsync/domainName:DomainName",
+              "aws:appsync/domainNameApiAssociation:DomainNameApiAssociation",
+              "aws:ec2/routeTableAssociation:RouteTableAssociation",
+              "aws:iam/accessKey:AccessKey",
+              "aws:iam/policy:Policy",
+              "aws:iam/userPolicy:UserPolicy",
+              "aws:cloudfront/cachePolicy:CachePolicy",
+              "aws:cloudfront/distribution:Distribution",
+              "aws:cloudfront/originAccessIdentity:OriginAccessIdentity",
+              "aws:cloudwatch/eventRule:EventRule",
+              "aws:cloudwatch/eventTarget:EventTarget",
+              "aws:cloudwatch/logGroup:LogGroup",
+              "aws:cognito/identityPoolRoleAttachment:IdentityPoolRoleAttachment",
+              "aws:cognito/userPoolClient:UserPoolClient",
+              "aws:lambda/eventSourceMapping:EventSourceMapping",
+              "aws:lambda/functionUrl:FunctionUrl",
+              "aws:lambda/invocation:Invocation",
+              "aws:lambda/permission:Permission",
+              "aws:lb/listener:Listener",
+              "aws:route53/record:Record",
+              "aws:s3/bucketCorsConfigurationV2:BucketCorsConfigurationV2",
+              "aws:s3/bucketNotification:BucketNotification",
+              "aws:s3/bucketObject:BucketObject",
+              "aws:s3/bucketObjectv2:BucketObjectv2",
+              "aws:s3/bucketPolicy:BucketPolicy",
+              "aws:s3/bucketPublicAccessBlock:BucketPublicAccessBlock",
+              "aws:s3/bucketWebsiteConfigurationV2:BucketWebsiteConfigurationV2",
+              "aws:ses/domainIdentityVerification:DomainIdentityVerification",
+              "aws:sesv2/emailIdentity:EmailIdentity",
+              "aws:sns/topicSubscription:TopicSubscription",
+              "cloudflare:index/record:Record",
+              "cloudflare:index/workerDomain:WorkerDomain",
+              "docker:index/image:Image",
+              "vercel:index/dnsRecord:DnsRecord",
+            ].includes(args.type)
+          )
+            return;
 
-          let overrides;
-          switch (args.type) {
-            // AWS LoadBalancer resource names allow for 32 chars, but an 8 letter suffix
-            // ie. "-1234567" is automatically added
-            case "aws:lb/loadBalancer:LoadBalancer":
-              overrides = { name: prefixName(24, args.name) };
-              break;
-            case "aws:rds/cluster:Cluster":
-              overrides = {
-                clusterIdentifier: prefixName(63, args.name).toLowerCase(),
-              };
-              break;
-            case "aws:rds/clusterInstance:ClusterInstance":
-              overrides = {
-                identifier: prefixName(63, args.name).toLowerCase(),
-              };
-              break;
-            case "aws:cloudwatch/eventRule:EventRule":
-            case "aws:cloudfront/function:Function":
-            case "aws:iam/user:User":
-              overrides = { name: prefixName(64, args.name) };
-              break;
-            case "aws:lambda/function:Function":
-              if (args.props.name) break;
-              overrides = { name: prefixName(64, args.name) };
-              break;
-            case "aws:sqs/queue:Queue":
-              overrides = {
-                name: output(args.props.fifoQueue).apply((fifo) =>
+          const namingRules = [
+            {
+              // AWS LoadBalancer resource names allow for 32 chars, but an 8 letter suffix
+              // ie. "-1234567" is automatically added
+              types: ["aws:lb/loadBalancer:LoadBalancer"],
+              field: "name",
+              cb: () => prefixName(24, args.name),
+            },
+            {
+              types: ["aws:rds/cluster:Cluster"],
+              field: "clusterIdentifier",
+              cb: () => prefixName(63, args.name).toLowerCase(),
+            },
+            {
+              types: ["aws:rds/clusterInstance:ClusterInstance"],
+              field: "identifier",
+              cb: () => prefixName(63, args.name).toLowerCase(),
+            },
+            {
+              types: [
+                "aws:cloudwatch/eventRule:EventRule",
+                "aws:cloudfront/function:Function",
+                "aws:iam/user:User",
+                "aws:lambda/function:Function",
+              ],
+              field: "name",
+              cb: () => prefixName(64, args.name),
+            },
+            {
+              types: ["aws:sqs/queue:Queue"],
+              field: "name",
+              cb: () =>
+                output(args.props.fifoQueue).apply((fifo) =>
                   prefixName(80, args.name, fifo ? ".fifo" : undefined),
                 ),
-              };
-              break;
-            case "aws:iam/role:Role":
-              overrides = {
-                name: getRegionOutput(undefined, {
+            },
+            {
+              types: ["aws:iam/role:Role"],
+              field: "name",
+              cb: () =>
+                getRegionOutput(undefined, {
                   provider: args.opts.provider,
                 }).name.apply((region) =>
                   prefixName(
@@ -110,135 +183,97 @@ export class Component extends ComponentResource {
                     `-${region.toLowerCase().replace(/-/g, "")}`,
                   ),
                 ),
-              };
-              break;
-            case "aws:apigateway/authorizer:Authorizer":
-            case "aws:apigateway/restApi:RestApi":
-            case "aws:apigatewayv2/api:Api":
-            case "aws:apigatewayv2/authorizer:Authorizer":
-            case "aws:cognito/userPool:UserPool":
-            case "aws:iot/authorizer:Authorizer":
-              overrides = { name: prefixName(128, args.name) };
-              break;
-            case "aws:iot/topicRule:TopicRule":
-              overrides = {
-                name: prefixName(128, args.name).replaceAll("-", "_"),
-              };
-              break;
-            case "aws:appautoscaling/policy:Policy":
-            case "aws:dynamodb/table:Table":
-            case "aws:kinesis/stream:Stream":
-            case "aws:ecs/cluster:Cluster":
-              overrides = { name: prefixName(255, args.name) };
-              break;
-            case "aws:rds/subnetGroup:SubnetGroup":
-              overrides = { name: prefixName(255, args.name).toLowerCase() };
-              break;
-            case "aws:ec2/eip:Eip":
-            case "aws:ec2/internetGateway:InternetGateway":
-            case "aws:ec2/natGateway:NatGateway":
-            case "aws:ec2/routeTable:RouteTable":
-            case "aws:ec2/securityGroup:SecurityGroup":
-            case "aws:ec2/subnet:Subnet":
-            case "aws:ec2/vpc:Vpc":
-              overrides = {
-                tags: {
-                  // @ts-expect-error
-                  ...args.tags,
-                  Name: prefixName(255, args.name),
-                },
-              };
-              break;
-            case "aws:sns/topic:Topic":
-              overrides = {
-                name: output(args.props.fifoTopic).apply((fifo) =>
+            },
+            {
+              types: [
+                "aws:apigateway/authorizer:Authorizer",
+                "aws:apigateway/restApi:RestApi",
+                "aws:apigatewayv2/api:Api",
+                "aws:apigatewayv2/authorizer:Authorizer",
+                "aws:cognito/userPool:UserPool",
+                "aws:iot/authorizer:Authorizer",
+              ],
+              field: "name",
+              cb: () => prefixName(128, args.name),
+            },
+            {
+              types: ["aws:iot/topicRule:TopicRule"],
+              field: "name",
+              cb: () => prefixName(128, args.name).replaceAll("-", "_"),
+            },
+            {
+              types: [
+                "aws:appautoscaling/policy:Policy",
+                "aws:dynamodb/table:Table",
+                "aws:kinesis/stream:Stream",
+                "aws:ecs/cluster:Cluster",
+              ],
+              field: "name",
+              cb: () => prefixName(255, args.name),
+            },
+            {
+              types: ["aws:rds/subnetGroup:SubnetGroup"],
+              field: "name",
+              cb: () => prefixName(255, args.name).toLowerCase(),
+            },
+            {
+              types: [
+                "aws:ec2/eip:Eip",
+                "aws:ec2/internetGateway:InternetGateway",
+                "aws:ec2/natGateway:NatGateway",
+                "aws:ec2/routeTable:RouteTable",
+                "aws:ec2/securityGroup:SecurityGroup",
+                "aws:ec2/subnet:Subnet",
+                "aws:ec2/vpc:Vpc",
+              ],
+              field: "tags",
+              cb: () => ({
+                // @ts-expect-error
+                ...args.tags,
+                Name: prefixName(255, args.name),
+              }),
+            },
+            {
+              types: ["aws:sns/topic:Topic"],
+              field: "name",
+              cb: () =>
+                output(args.props.fifoTopic).apply((fifo) =>
                   prefixName(256, args.name, fifo ? ".fifo" : undefined),
                 ),
-              };
-              break;
-            case "aws:appsync/graphQLApi:GraphQLApi":
-              overrides = { name: prefixName(65536, args.name) };
-              break;
-            case "cloudflare:index/d1Database:D1Database":
-            case "cloudflare:index/r2Bucket:R2Bucket":
-            case "cloudflare:index/workerScript:WorkerScript":
-            case "cloudflare:index/queue:Queue":
-              overrides = {
-                name: prefixName(64, args.name).toLowerCase(),
-              };
-              break;
-            case "cloudflare:index/workersKvNamespace:WorkersKvNamespace":
-              overrides = {
-                title: prefixName(64, args.name).toLowerCase(),
-              };
-              break;
-            // resources manually named
-            case "aws:appsync/dataSource:DataSource":
-            case "aws:appsync/function:Function":
-            case "aws:appsync/resolver:Resolver":
-            case "aws:cognito/identityPool:IdentityPool":
-            case "aws:ecs/service:Service":
-            case "aws:ecs/taskDefinition:TaskDefinition":
-            case "aws:lb/targetGroup:TargetGroup":
-            case "aws:s3/bucketV2:BucketV2":
-            case "aws:cloudwatch/eventBus:EventBus":
-              break;
-            // resources not prefixed
-            case "aws:acm/certificate:Certificate":
-            case "aws:acm/certificateValidation:CertificateValidation":
-            case "aws:apigateway/deployment:Deployment":
-            case "aws:apigateway/integration:Integration":
-            case "aws:apigateway/method:Method":
-            case "aws:apigateway/resource:Resource":
-            case "aws:apigateway/stage:Stage":
-            case "aws:apigatewayv2/apiMapping:ApiMapping":
-            case "aws:apigatewayv2/domainName:DomainName":
-            case "aws:apigatewayv2/integration:Integration":
-            case "aws:apigatewayv2/route:Route":
-            case "aws:apigatewayv2/stage:Stage":
-            case "aws:appautoscaling/target:Target":
-            case "aws:appsync/domainName:DomainName":
-            case "aws:appsync/domainNameApiAssociation:DomainNameApiAssociation":
-            case "aws:ec2/routeTableAssociation:RouteTableAssociation":
-            case "aws:iam/accessKey:AccessKey":
-            case "aws:iam/policy:Policy":
-            case "aws:iam/userPolicy:UserPolicy":
-            case "aws:cloudfront/cachePolicy:CachePolicy":
-            case "aws:cloudfront/distribution:Distribution":
-            case "aws:cloudfront/originAccessIdentity:OriginAccessIdentity":
-            case "aws:cloudwatch/eventRule:EventRule":
-            case "aws:cloudwatch/eventTarget:EventTarget":
-            case "aws:cloudwatch/logGroup:LogGroup":
-            case "aws:cognito/identityPoolRoleAttachment:IdentityPoolRoleAttachment":
-            case "aws:cognito/userPoolClient:UserPoolClient":
-            case "aws:lambda/eventSourceMapping:EventSourceMapping":
-            case "aws:lambda/functionUrl:FunctionUrl":
-            case "aws:lambda/invocation:Invocation":
-            case "aws:lambda/permission:Permission":
-            case "aws:lb/listener:Listener":
-            case "aws:route53/record:Record":
-            case "aws:s3/bucketCorsConfigurationV2:BucketCorsConfigurationV2":
-            case "aws:s3/bucketNotification:BucketNotification":
-            case "aws:s3/bucketObject:BucketObject":
-            case "aws:s3/bucketObjectv2:BucketObjectv2":
-            case "aws:s3/bucketPolicy:BucketPolicy":
-            case "aws:s3/bucketPublicAccessBlock:BucketPublicAccessBlock":
-            case "aws:s3/bucketWebsiteConfigurationV2:BucketWebsiteConfigurationV2":
-            case "aws:ses/domainIdentityVerification:DomainIdentityVerification":
-            case "aws:sesv2/emailIdentity:EmailIdentity":
-            case "aws:sns/topicSubscription:TopicSubscription":
-            case "cloudflare:index/record:Record":
-            case "cloudflare:index/workerDomain:WorkerDomain":
-            case "docker:index/image:Image":
-            case "vercel:index/dnsRecord:DnsRecord":
-              break;
-            default:
-              throw new VisibleError(
-                `In "${name}" component, the physical name of "${args.name}" (${args.type}) is not prefixed`,
-              );
-          }
+            },
+            {
+              types: ["aws:appsync/graphQLApi:GraphQLApi"],
+              field: "name",
+              cb: () => prefixName(65536, args.name),
+            },
+            {
+              types: [
+                "cloudflare:index/d1Database:D1Database",
+                "cloudflare:index/r2Bucket:R2Bucket",
+                "cloudflare:index/workerScript:WorkerScript",
+                "cloudflare:index/queue:Queue",
+              ],
+              field: "name",
+              cb: () => prefixName(64, args.name).toLowerCase(),
+            },
+            {
+              types: ["cloudflare:index/workersKvNamespace:WorkersKvNamespace"],
+              field: "title",
+              cb: () => prefixName(64, args.name).toLowerCase(),
+            },
+          ];
+
+          const rule = namingRules.find((r) => r.types.includes(args.type));
+          if (!rule)
+            throw new VisibleError(
+              `In "${name}" component, the physical name of "${args.name}" (${args.type}) is not prefixed`,
+            );
+
+          // name is already set
+          if (args.props[rule.field]) return;
+
           return {
-            props: { ...args.props, ...overrides },
+            props: { ...args.props, [rule.field]: rule.cb() },
             opts: args.opts,
           };
         },
