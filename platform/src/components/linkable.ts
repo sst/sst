0a81383f66ac161b2a2559938a4c6f63e693aa4f
Jay V <air@live.ca>
2024-07-30 17:16:28 -0400

    docs: linkable

--- platform/src/components/linkable.ts
+++ platform/src/components/linkable.ts
@@ -6,31 +6,37 @@ export interface Definition<
   Properties extends Record<string, any> = Record<string, any>,
 > {
   /**
-   * Defining values that can be linked in your app.
+   * Define values that the linked resource can access at runtime. These can be outputs
+   * from other resources or constants.
+   *
    * @example
    * ```ts
    * {
-   *   properties: { foo: "bar" },
+   *   properties: { foo: "bar" }
    * }
    * ```
    */
   properties: Properties;
   /**
-   * Defining AWS permissions or Cloudflare bindings for the linkable resource.
+   * Include AWS permissions or Cloudflare bindings for the linkable resource. The linked
+   * resource will have these permissions or bindings.
+   *
    * @example
-   * Defining AWS permissions.
+   * Include AWS permissions.
+   *
    * ```ts
    * {
    *   include: [
    *     sst.aws.permission({
    *       actions: ["lambda:InvokeFunction"],
-   *       resources: ["*"],
-   *     }),
-   *   ],
+   *       resources: ["*"]
+   *     })
+   *   ]
    * }
    * ```
    *
-   * Defining Cloudflare bindings.
+   * Include Cloudflare bindings.
+   *
    * ```ts
    * {
    *   include: [
@@ -51,28 +57,113 @@ export interface Definition<
 }
 
 /**
- * The `Linkable` component lets you link values in your app.
+ * The `Linkable` component and the `Linkable.wrap` method lets you link any resources in your
+ * app; not just the built-in SST components.
+ *
  * @example
- * Similar to linking an SST component, you can link any value in your app.
+ *
+ * #### Linking any value
+ *
+ * The `Linkable` component takes a list of properties that you want to link. These can be
+ * outputs from other resources or constants.
+ *
  * ```ts title="sst.config.ts"
- * const linkable = new sst.Linkable("MyLinkable",{
- *   properties: { foo: "bar" },
+ * new sst.Linkable("MyLinkable", {
+ *   properties: { foo: "bar" }
  * });
  * ```
  *
- * Then use the [SDK](/docs/reference/sdk/) to access the linked resource in your
- * runtime in a typesafe way.
+ * You can also use this to combine multiple resources into a single linkable resource.
+ *
+ * ```ts title="sst.config.ts"
+ * const bucketA = new sst.aws.Bucket("MyBucketA");
+ * const bucketB = new sst.aws.Bucket("MyBucketB");
+ *
+ * const storage = new sst.Linkable("MyStorage", {
+ *   properties: {
+ *     bucketA: bucketA.name,
+ *     bucketB: bucketB.name
+ *   }
+ * });
+ * ```
+ *
+ * You can optionally include permissions or bindings for the linked resource.
+ *
+ * ```ts title="sst.config.ts"
+ * const storage = new sst.Linkable("MyStorage", {
+ *   properties: {
+ *     bucketA: bucketA.name,
+ *     bucketB: bucketB.name
+ *   },
+ *   include: [
+ *     sst.aws.permission({
+ *       actions: ["s3:*"],
+ *       resources: [bucketA.arn, bucketB.arn]
+ *     })
+ *   ]
+ * });
+ * ```
+ *
+ * #### Link to a resource
+ *
+ * You can now link this resource to your frontend or a function.
+ *
+ * ```ts title="sst.config.ts" {3}
+ * new sst.aws.Function("MyApi", {
+ *   handler: "src/lambda.handler",
+ *   link: [storage]
+ * });
+ * ```
+ *
+ * Then use the [SDK](/docs/reference/sdk/) to access them at runtime.
  *
  * ```js title="src/lambda.ts"
  * import { Resource } from "sst";
  *
- * console.log(Resource.MyLinkable.foo);
+ * console.log(Resource.MyStorage.bucketA);
+ * ```
+ *
+ * #### Linking a Pulumi Resource
+ *
+ * You can also wrap any Pulumi Resource class to make it linkable.
+ *
+ * ```ts title="sst.config.ts"
+ * Linkable.wrap(aws.dynamodb.Table, (table) => ({
+ *   properties: { tableName: table.name },
+ *   include: [
+ *     sst.aws.permission({
+ *       actions: ["dynamodb:*"],
+ *       resources: [table.arn]
+ *     })
+ *   ]
+ * }));
+ * ```
+ *
+ * Now you create an instance of `aws.dynamodb.Table` and link it in your app like any other SST
+ * component.
+ *
+ * ```ts title="sst.config.ts" {7}
+ * const table = new aws.dynamodb.Table("MyTable", {
+ *   attributes: [{ name: "id", type: "S" }],
+ *   hashKey: "id"
+ * });
+ *
+ * new sst.aws.Nextjs("MyWeb", {
+ *   link: [table]
+ * });
+ * ```
+ *
+ * And use the [SDK](/docs/reference/sdk/) to access it at runtime.
+ *
+ * ```js title="app/page.tsx"
+ * import { Resource } from "sst";
+ *
+ * console.log(Resource.MyTable.tableName);
  * ```
  */
 export class Linkable<T extends Record<string, any>>
   extends Component
-  implements Link.Linkable
-{
+  implements Link.Linkable {
   private _name: string;
   private _definition: Definition<T>;
 
@@ -96,29 +187,34 @@ export class Linkable<T extends Record<string, any>>
   }
 
   /**
-   * Wrap a Pulumi Resource class to make it linkable.
+   * Wrap any Pulumi Resource class to make it linkable. Behind the scenes this modifies the
+   * prototype of the given class.
    *
    * @param cls The Pulumi Resource class to wrap.
    * @param cb A callback that returns the definition for the linkable resource.
    *
    * @example
-   * The following example wraps the [`aws.dynamodb.Table`](https://www.pulumi.com/registry/packages/aws/api-docs/dynamodb/table/)
+   *
+   * Here we are wrapping the [`aws.dynamodb.Table`](https://www.pulumi.com/registry/packages/aws/api-docs/dynamodb/table/)
    * class to make it linkable.
    *
    * ```ts title="sst.config.ts"
    * Linkable.wrap(aws.dynamodb.Table, (table) => ({
    *   properties: { tableName: table.name },
    *   include: [
-   *     permission({
+   *     sst.aws.permission({
    *       actions: ["dynamodb:*"],
-   *       resources: [table.arn, interpolate`${table.arn}/`],
-   *     }),
-   *   ],
+   *       resources: [table.arn]
+   *     })
+   *   ]
    * }));
    * ```
    *
-   * Then you can link any `aws.dynamodb.Table` instances in your app as if they were SST
-   * components.
+   * It's defining the properties that we want made accessible at runtime and the permissions
+   * that the linked resource should have.
+   *
+   * Now you can link any `aws.dynamodb.Table` instances in your app just like any other SST
+   * component.
    *
    * ```ts title="sst.config.ts" {7}
    * const table = new aws.dynamodb.Table("MyTable", {
@@ -132,10 +228,10 @@ export class Linkable<T extends Record<string, any>>
    * ```
    */
   public static wrap<Resource>(
-    cls: { new (...args: any[]): Resource },
+    cls: { new(...args: any[]): Resource },
     cb: (resource: Resource) => Definition,
   ) {
-    cls.prototype.getSSTLink = function () {
+    cls.prototype.getSSTLink = function() {
       return cb(this);
     };
   }
