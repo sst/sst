373e5b5a216aa82d7d8ca9644c9931f7dbd00855
Frank <frank@sst.dev>
2024-08-16 18:26:33 -0400

    docs: add url redirect

--- www/sst.config.ts
+++ www/sst.config.ts
@@ -83,8 +83,67 @@ export default $config({
       });
     }
 
+    // Redirect /examples to guide.sst.dev/examples
+    // Redirect /chapters to guide.sst.dev/chapters
+    // Redirect /archives to guide.sst.dev/archives
+    const cfFunction = new aws.cloudfront.Function("AstroRedirect", {
+      runtime: "cloudfront-js-2.0",
+      code: [
+        `function handler(event) {`,
+        `  const request = event.request;`,
+        // ie. request.uri is /examples/foo
+        `  return {`,
+        `    statusCode: 302,`,
+        `    statusDescription: 'Found',`,
+        `    headers: {`,
+        `      location: { value: "https://guide.sst.dev" + request.uri }`,
+        `    },`,
+        `  };`,
+        `}`,
+      ].join("\n"),
+    });
+    const behaviorConfig = {
+      targetOriginId: "redirect",
+      viewerProtocolPolicy: "redirect-to-https",
+      allowedMethods: ["GET", "HEAD", "OPTIONS"],
+      cachedMethods: ["GET", "HEAD"],
+      functionAssociations: [
+        { eventType: "viewer-request", functionArn: cfFunction.arn },
+      ],
+      forwardedValues: {
+        queryString: true,
+        headers: ["Origin"],
+        cookies: { forward: "none" },
+      },
+    };
     new sst.aws.Astro("Astro", {
       domain,
+      transform: {
+        cdn: (args) => {
+          args.origins = $output(args.origins).apply((origins) => [
+            ...origins,
+            {
+              domainName: "guide.sst.dev",
+              originId: "redirect",
+              customOriginConfig: {
+                httpPort: 80,
+                httpsPort: 443,
+                originProtocolPolicy: "https-only",
+                originReadTimeout: 20,
+                originSslProtocols: ["TLSv1.2"],
+              },
+            },
+          ]);
+          args.orderedCacheBehaviors = $output(
+            args.orderedCacheBehaviors
+          ).apply((cacheBehaviors) => [
+            ...(cacheBehaviors || []),
+            { pathPattern: "/examples*", ...behaviorConfig },
+            { pathPattern: "/chapters*", ...behaviorConfig },
+            { pathPattern: "/archives*", ...behaviorConfig },
+          ]);
+        },
+      },
     });
 
     new sst.aws.Router("TelemetryRouter", {
