bde63a7a930222795c9afbb03c3ced53c3e6e8ad
Dax Raad <d@ironbay.co>
2024-08-20 14:59:36 -0400

    ignore: missing commit

--- pkg/flag/flag.go
+++ pkg/flag/flag.go
@@ -7,3 +7,4 @@ import (
 var SST_NO_CLEANUP = os.Getenv("SST_NO_CLEANUP") != ""
 var SST_PASSPHRASE = os.Getenv("SST_PASSPHRASE")
 var SST_PULUMI_PATH = os.Getenv("SST_PULUMI_PATH")
+var SST_PRINT_LOGS = os.Getenv("SST_PRINT_LOGS") != ""
