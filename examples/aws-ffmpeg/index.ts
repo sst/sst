8674eb9577958530bfb8e13118243fcda7724ac6
Jay V <air@live.ca>
2024-09-04 18:42:27 -0400

    docs: ffmpeg

---
+++ examples/aws-ffmpeg/index.ts
@@ -0,0 +1,40 @@
+import path from "path";
+import ffmpeg from "ffmpeg-static";
+import { promises as fs } from "fs";
+import { spawnSync } from "child_process";
+
+export async function handler() {
+  const videoPath = "clip.mp4";
+
+  const outputFile = "thumbnail.jpg";
+  const outputPath = process.env.SST_LIVE
+    ? outputFile
+    : path.join("/tmp", outputFile);
+
+  const ffmpegParams = [
+    "-ss",
+    "1",
+    "-i",
+    videoPath,
+    "-vf",
+    "thumbnail,scale=960:540",
+    "-vframes",
+    "1",
+    outputPath,
+  ];
+
+  spawnSync(ffmpeg, ffmpegParams, { stdio: "pipe" });
+
+  const img = await fs.readFile(outputPath);
+  const body = Buffer.from(img).toString("base64");
+
+  return {
+    body,
+    statusCode: 200,
+    isBase64Encoded: true,
+    headers: {
+      "Content-Type": "image/jpeg",
+      "Content-Disposition": "inline"
+    },
+  };
+}
---
+++ examples/aws-ffmpeg/sst.config.ts
@@ -0,0 +1,57 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+/**
+ * ## FFmpeg in Lambda
+ *
+ * Uses [FFmpeg](https://ffmpeg.org/) to process videos. In this example, it takes a `clip.mp4`
+ * and grabs a single frame from it.
+ *
+ * :::tip
+ * You don't need to use a Lambda layer to use FFmpeg.
+ * :::
+ *
+ * We use the [`ffmpeg-static`](https://www.npmjs.com/package/ffmpeg-static) package that
+ * contains pre-built binaries for all architectures.
+ *
+ * ```ts title="index.ts"
+ * import ffmpeg from "ffmpeg-static";
+ * ```
+ *
+ * We can use this to spawn a child process and run FFmpeg.
+ *
+ * ```ts title="index.ts"
+ * spawnSync(ffmpeg, ffmpegParams, { stdio: "pipe" });
+ * ```
+ *
+ * We don't need a layer when we deploy this because SST will use the right binary for the
+ * target Lambda architecture.
+ *
+ * ```json title="sst.config.ts"
+ * {
+ *   nodejs: { install: ["ffmpeg-static"] }
+ * }
+ * ```
+ *
+ * This is handled by [`nodejs.install`](/docs/component/aws/function#nodejs-install).
+ */
+export default $config({
+  app(input) {
+    return {
+      name: "aws-ffmpeg",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
+    };
+  },
+  async run() {
+    const func = new sst.aws.Function("MyFunction", {
+      url: true,
+      handler: "index.handler",
+      copyFiles: [{ from: "clip.mp4" }],
+      nodejs: { install: ["ffmpeg-static"] },
+    });
+
+    return {
+      url: func.url,
+    };
+  },
+});
