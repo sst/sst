dd8d0e394ca60043487984894df9fec7dde6700a
Jay V <air@live.ca>
2024-09-05 12:58:24 -0400

    docs: examples

--- examples/aws-multi-region/sst.config.ts
+++ examples/aws-multi-region/sst.config.ts
@@ -1,12 +1,5 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
-/**
- * ## Current AWS account
- *
- * You can use the `aws.getXXXXOutput()` provider functions to get info about the current
- * AWS account.
- * Learn more about [provider functions](/docs/providers/#functions).
- */
 export default $config({
   app(input) {
     return {
