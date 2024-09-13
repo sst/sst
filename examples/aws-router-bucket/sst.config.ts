d2de5041a4ebe8877deb71c540660496e01be840
Frank <frank@sst.dev>
2024-09-13 13:23:14 -0400

    examples: router and bucket

---
+++ examples/aws-router-bucket/sst.config.ts
@@ -0,0 +1,43 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+import path from "path";
+
+/**
+ * ## AWS Router and bucket
+ *
+ * Creates a router that serves static files from the `public` folder in a bucket.
+ */
+export default $config({
+  app(input) {
+    return {
+      name: "aws-router-bucket",
+      home: "aws",
+      removal: input?.stage === "production" ? "retain" : "remove",
+    };
+  },
+  async run() {
+    // Create a bucket that CloudFront can access
+    const bucket = new sst.aws.Bucket("MyBucket", {
+      access: "cloudfront",
+    });
+
+    // Upload the image to the `public` folder
+    new aws.s3.BucketObjectv2("MyImage", {
+      bucket: bucket.name,
+      key: "public/spongebob.svg",
+      contentType: "image/svg+xml",
+      source: new $util.asset.FileAsset(
+        path.join($cli.paths.root, "spongebob.svg")
+      ),
+    });
+
+    new sst.aws.Router("MyRouter", {
+      routes: {
+        "/*": {
+          bucket,
+          rewrite: { regex: "^/(.*)$", to: "/public/$1" },
+        },
+      },
+    });
+  },
+});
