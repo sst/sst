5b779e26b3158742c7b0015ceceeba0727045fe8
Jay V <air@live.ca>
2024-09-26 15:37:55 -0400

    docs: registry env var

--- cmd/sst/main.go
+++ cmd/sst/main.go
@@ -591,6 +591,12 @@ var root = &cli.Command{
 					"```",
 					"",
 					"You'll need to run `sst install` if you update the `providers` in your config.",
+					"",
+					"By default these packages are fetched from the NPM registry. If you want to use a different registry, you can set the `NPM_REGISTRY` environment variable.",
+					"",
+					"```bash frame=\"none\"",
+					"NPM_REGISTRY=https://my-registry.com sst add aws",
+					"```",
 				}, "\n"),
 			},
 			Args: []cli.Argument{
