619b199b6bbfc89a43ce3fb78fa6b40450a29d33
Jay V <air@live.ca>
2024-09-27 15:30:17 -0400

    docs: elysia

--- examples/aws-bun-elysia/sst.config.ts
+++ examples/aws-bun-elysia/sst.config.ts
@@ -1,5 +1,46 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
+/**
+ * ## AWS Bun Elysia container
+ *
+ * Deploys a Bun [Elysia](https://elysiajs.com/) API to AWS.
+ *
+ * You can get started by running.
+ *
+ * ```bash
+ * bun create elysia aws-bun-elysia
+ * cd aws-bun-elysia
+ * bunx sst init
+ * ```
+ *
+ * Now you can add a service.
+ *
+ * ```ts title="sst.config.ts"
+ * cluster.addService("MyService", {
+ *   public: {
+ *     ports: [{ listen: "80/http", forward: "3000/http" }],
+ *   },
+ *   dev: {
+ *     command: "bun dev",
+ *   },
+ * });
+ * ```
+ *
+ * Start your app locally.
+ *
+ * ```bash
+ * bun sst dev
+ * ```
+ *
+ * This example lets you upload a file to S3 and then download it.
+ *
+ * ```bash
+ * curl --F file=@elysia.png http://localhost:3000/
+ * curl http://localhost:3000/latest
+ * ```
+ *
+ * Finally, you can deploy it using `bun sst deploy`.
+ */
 export default $config({
   app(input) {
     return {
