3e6745ceeb4fb4dfe804be16574f572e273f1454
Victor Ndaba <mwangindaba@gmail.com>
2024-10-13 00:18:34 +0300

    docs: update `drizzle` example code and docs

--- examples/aws-drizzle/drizzle.config.ts
+++ examples/aws-drizzle/drizzle.config.ts
@@ -2,13 +2,7 @@ import { Resource } from "sst";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  driver: "aws-data-api",
   dialect: "postgresql",
-  dbCredentials: {
-    database: Resource.MyPostgres.database,
-    secretArn: Resource.MyPostgres.secretArn,
-    resourceArn: Resource.MyPostgres.clusterArn,
-  },
   // Pick up all our schema files
   schema: ["./src/**/*.sql.ts"],
   out: "./migrations",
--- examples/aws-drizzle/src/drizzle.ts
+++ examples/aws-drizzle/src/drizzle.ts
@@ -1,13 +1,15 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 import { Resource } from "sst";
-import { drizzle } from "drizzle-orm/aws-data-api/pg";
-import { RDSDataClient } from "@aws-sdk/client-rds-data";
 import * as schema from "./todo.sql";
 
-const client = new RDSDataClient({});
-
-export const db = drizzle(client, {
-  schema,
-  database: Resource.MyPostgres.database,
-  secretArn: Resource.MyPostgres.secretArn,
-  resourceArn: Resource.MyPostgres.clusterArn,
+const cfg = Resource.MyPostgres;
+const pool = new Pool({
+  host: cfg.host,
+  port: cfg.port,
+  user: cfg.username,
+  password: cfg.password,
+  database: cfg.database,
 });
+
+export const db = drizzle(pool, { schema });
