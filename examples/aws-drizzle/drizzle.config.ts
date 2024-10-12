2d8770d0552ede46c60cd5a1704be89d10ec3afa
Victor Ndaba <mwangindaba@gmail.com>
2024-10-13 00:59:01 +0300

    docs: start tunnel before migration

--- examples/aws-drizzle/drizzle.config.ts
+++ examples/aws-drizzle/drizzle.config.ts
@@ -1,9 +1,18 @@
 import { Resource } from "sst";
 import { defineConfig } from "drizzle-kit";
 
+const cfg = Resource.MyPostgres;
+
 export default defineConfig({
   dialect: "postgresql",
   // Pick up all our schema files
   schema: ["./src/**/*.sql.ts"],
   out: "./migrations",
+  dbCredentials: {
+    host: cfg.host,
+    port: cfg.port,
+    user: cfg.username,
+    password: cfg.password,
+    database: cfg.database,
+  },
 });
