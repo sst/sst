8ea2d5e3c9f77c4d5b190514262180d44dd68e1c
Jay V <air@live.ca>
2024-10-21 17:57:42 -0400

    docs: adding deno back

---
+++ examples/aws-deno/main.ts
@@ -0,0 +1,19 @@
+import { Resource } from "sst";
+import { Cluster } from "ioredis";
+
+const redis = new Cluster(
+  [{ host: Resource.MyRedis.host, port: Resource.MyRedis.port }],
+  {
+    dnsLookup: (address, callback) => callback(null, address),
+    redisOptions: {
+      tls: {},
+      username: Resource.MyRedis.username,
+      password: Resource.MyRedis.password,
+    },
+  },
+);
+
+Deno.serve(async (_req) => {
+  const counter = await redis.incr("counter");
+  return new Response(`Hit counter: ${counter}`);
+});
