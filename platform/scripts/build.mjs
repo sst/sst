be2804126669c64851972c39bdcfd5f09f976c0f
arjunyel <arjunyel@gmail.com>
2026-08-08 23:49:37 -0500

    Remove banner from oac-edge-signer

--- platform/scripts/build.mjs
+++ platform/scripts/build.mjs
@@ -11,12 +11,16 @@ await Promise.all(
       target: "esnext",
       format: "esm",
       entryPoints: [`./functions/${file}/index.ts`],
-      banner: {
-        js: [
-          `import { createRequire as topLevelCreateRequire } from 'module';`,
-          `const require = topLevelCreateRequire(import.meta.url);`,
-        ].join(""),
-      },
+      ...(file !== "oac-edge-signer"
+        ? {
+            banner: {
+              js: [
+                `import { createRequire as topLevelCreateRequire } from 'module';`,
+                `const require = topLevelCreateRequire(import.meta.url);`,
+              ].join(""),
+            },
+          }
+        : {}),
       outfile: `./dist/${file}/index.mjs`,
     }),
   ),
