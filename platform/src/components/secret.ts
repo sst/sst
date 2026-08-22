df5e02979053dc6fb57ad3a1f5633d0910333ff7
MeGaurav4 <gaurav3.141592@gmail.com>
2026-08-22 11:14:18 +0530

    docs(secret): drop VideoAside linking a private video
    The "Watch a video on how secrets work" link points to a private
    YouTube video, so viewers hit "This is a private video". Remove the
    aside until a replacement is available.
    
    Closes #6975
    
    Signed-off-by: MeGaurav4 <gaurav3.141592@gmail.com>

--- platform/src/components/secret.ts
+++ platform/src/components/secret.ts
@@ -15,7 +15,6 @@ export class SecretMissingError extends VisibleError {
 /**
  * The `Secret` component lets you create secrets in your app.
  *
- * <VideoAside title="Watch a video on how secrets work" href="https://youtu.be/7tW2L3P6LKw" />
  *
  * Secrets are encrypted and stored in an S3 Bucket in your AWS account. If used in
  * your app config, they'll be encrypted in your state file as well. If used in
